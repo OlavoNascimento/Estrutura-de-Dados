53f69ce8d321f28dd45fddc5d146d0f3998ad5ea
Olavo Nascimento <olavo.nascimento4@gmail.com>
2020-07-12 16:27:14 -0300

    Remover struct Circulo do arquivo .h e criar getters.
    Move o struct que define um circulo do seu arquivo .h para seu arquivo .c, alterando a declaração no .h para um "typedef *void". Também adiciona getters para acessar os dados do struct.

--- src/circulo.c
+++ src/circulo.c
@@ -1,8 +1,17 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
 #include <circulo.h>
+
+typedef struct {
+    char id[100];
+    double raio;
+    double x;
+    double y;
+    char cor_borda[20];
+    char cor_preenchimento[20];
+} Circulo;
 
 // Criar um círculo com base em informações de uma linha.
 Circulo ler_circulo(const char *linha) {
@@ -10,26 +19,24 @@ Circulo ler_circulo(const char *linha) {
     sscanf(
         linha,
         "%*c %s %lf %lf %lf %s %s",
-        circ.id, &circ.raio, &circ.x, &circ.y, circ.cor_borda, circ.cor_preenchimento
-    );
+        circ.id, &circ.raio, &circ.x, &circ.y, circ.cor_borda, circ.cor_preenchimento);
     return circ;
 }
 
 // Escreve todos os dados de um círculo em um arquivo.
 void escrever_informacoes_circulo(FILE *arquivo, Circulo circ) {
-    if(strlen(circ.id) > 0)
+    if (strlen(circ.id) > 0)
         fprintf(arquivo, "id: %s, ", circ.id);
     fprintf(
         arquivo,
         "raio: %lf, x: %lf, y: %lf, corb: %s, corp: %s\n",
-        circ.raio, circ.x, circ.y, circ.cor_borda, circ.cor_preenchimento
-    );
+        circ.raio, circ.x, circ.y, circ.cor_borda, circ.cor_preenchimento);
 }
 
 // Escreve o código svg que representa um círculo em um arquivo.
 void escrever_svg_circulo(FILE *arquivo, Circulo circ) {
     fprintf(arquivo, "\t<circle ");
-    if(strlen(circ.id) > 0)
+    if (strlen(circ.id) > 1)
         fprintf(arquivo, "id='%s' ", circ.id);
     fprintf(arquivo,
             "r='%lf' cx='%lf' cy='%lf' stroke='%s' fill='%s'/>\n",
@@ -37,8 +44,7 @@ void escrever_svg_circulo(FILE *arquivo, Circulo circ) {
             circ.x,
             circ.y,
             circ.cor_borda,
-            circ.cor_preenchimento
-    );
+            circ.cor_preenchimento);
 }
 
 // Retorna verdadeiro se dois círculos se intersectam.
@@ -46,7 +52,7 @@ bool checar_interseccao_circulo(Circulo circ1, Circulo circ2) {
     double dist = (circ1.x - circ2.x) * (circ1.x - circ2.x) +
                   (circ1.y - circ2.y) * (circ1.y - circ2.y);
     double raios = (circ1.raio + circ2.raio) * (circ1.raio + circ2.raio);
-    if(dist > raios)
+    if (dist > raios)
         return false;
     return true;
 }
@@ -54,9 +60,33 @@ bool checar_interseccao_circulo(Circulo circ1, Circulo circ2) {
 // Retorna verdadeiro se um ponto se encontra dentro de um círculo.
 bool checar_ponto_interno_circulo(Circulo circ,
                                   double ponto_x, double ponto_y) {
-    if(ponto_x <= circ.x - circ.raio || ponto_x >= circ.x + circ.raio)
+    if (ponto_x <= circ.x - circ.raio || ponto_x >= circ.x + circ.raio)
         return false;
-    if(ponto_y <= circ.y - circ.raio || ponto_y >= circ.y + circ.raio)
+    if (ponto_y <= circ.y - circ.raio || ponto_y >= circ.y + circ.raio)
         return false;
     return true;
+}
+
+char *obterIdCirculo(Circulo circ) {
+    return circ.id;
+}
+
+double obterRaioCirculo(Circulo circ) {
+    return circ.raio;
+}
+
+double obterXCirculo(Circulo circ) {
+    return circ.x;
+}
+
+double obterYCirculo(Circulo circ) {
+    return circ.y;
+}
+
+char *obterCorBordaCirculo(Circulo circ) {
+    return circ.cor_borda;
+}
+
+char *obterCorPreenchimentoCirculo(Circulo circ) {
+    return circ.cor_preenchimento;
 }
--- src/circulo.h
+++ src/circulo.h
@@ -1,18 +1,33 @@
 #pragma once
 
+// Este módulo define o tipo Circulo, assim como funções que criam, acessam suas propiedades e realizam operações
+// exclusivas deste tipo.
+
+#include <stdbool.h>
 #include <stdio.h>
 
-typedef struct {
-    char id[100];
-    double raio;
-    double x;
-    double y;
-    char cor_borda[20];
-    char cor_preenchimento[20];
-} Circulo;
+typedef void *Circulo;
 
+// Cria e inicializa um struct Circulo a partir de um texto obtido no arquivo .geo. Parâmetro linha não pode ser nulo.
 Circulo ler_circulo(const char *linha);
+// Escreve todas as informações presentes em um círculo em um arquivo. Ambos os parâmetros não podem ser nulos.
 void escrever_informacoes_circulo(FILE *arquivo, Circulo circ);
+// Escreve o código svg necessário para representar um círculo em um arquivo. Ambos os parâmetros não podem ser nulos.
 void escrever_svg_circulo(FILE *arquivo, Circulo circ);
+// Recebe dois circulos e retorna verdadeiro caso se intersectem. Ambos os parâmetros não podem ser nulos.
 bool checar_interseccao_circulo(Circulo circ1, Circulo circ2);
+// Recebe um círculo e as coordenadas de um ponto, retorna verdadeiro caso o ponto esteja dentro do círculo. Ambos os
+// parâmetros não podem ser nulos.
 bool checar_ponto_interno_circulo(Circulo circ, double ponto_x, double ponto_y);
+// Retorna o id de um círculo. O parâmetro não pode ser nulo.
+char *obterIdCirculo(Circulo circ);
+// Retorna o raio de um círculo. O parâmetro não pode ser nulo.
+double obterRaioCirculo(Circulo circ);
+// Retorna a coordenada x de um círculo. O parâmetro não pode ser nulo.
+double obterXCirculo(Circulo circ);
+// Retorna a coordenada y de um círculo. O parâmetro não pode ser nulo.
+double obterYCirculo(Circulo circ);
+// Retorna a cor da borda de um círculo. O parâmetro não pode ser nulo.
+char *obterCorBordaCirculo(Circulo circ);
+// Retorna a cor de preenchimento de um círculo. O parâmetro não pode ser nulo.
+char *obterCorPreenchimentoCirculo(Circulo circ);
