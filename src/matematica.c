addfcb8219b25ba8e8ab7a962c3a37ff0c0f0877
Olavo Nascimento <olavo.nascimento4@gmail.com>
2020-08-10 00:06:57 -0300

    Adicionar módulo para funções matemáticas.
    Move funções matemáticas compartilhadas por mais de um módulo para um arquivo específico.

---
+++ src/matematica.c
@@ -0,0 +1,11 @@
+#include "matematica.h"
+
+// Retorna o maior valor de dois parâmetros
+double max(double a, double b) {
+    return a > b ? a : b;
+}
+
+// Retorna o menor valor de dois parâmetros
+double min(double a, double b) {
+    return a < b ? a : b;
+}
---
+++ src/matematica.h
@@ -0,0 +1,11 @@
+#ifndef MATEMATICA_H
+#define MATEMATICA_H
+
+// Este módulo define funções matemáticas que são utilizadas por mais de um módulo
+
+// Retorna o maior valor de dois parâmetros
+double max(double a, double b);
+// Retorna o menor valor de dois parâmetros
+double min(double a, double b);
+
+#endif
