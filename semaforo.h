b74ee44da9c18ed8ec611ef8694f4ade4141f7ad
Felipe Dallmann Tomazeli <56651954+felipedallmann@users.noreply.github.com>
2020-08-23 00:56:18 -0300

    adicionar comando del
    Permite o arquivo ler_qry.c interpretar e executar o comando del, permitindo deletar um determinado equipamento urbano e adicionar uma linha vertical do centro do equipamento ao topo da imagem e ao lado da linha escrever o cep ou id do equipamento.

---
+++ semaforo.h
@@ -0,0 +1,52 @@
+#ifndef SEMAFORO_H
+#define SEMAFORO_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "retangulo.h"
+
+typedef void *Semaforo;
+
+// Cria e inicializa um struct Semaforo com os valores passados.
+Semaforo semaforo_criar(char id[100], double largura, double altura, double x, double y,
+                        char cor_borda[20], char cor_preenchimento[20]);
+// Cria e inicializa um struct Semaforo(igual ao retângulo) a partir de um texto obtido no arquivo
+// .geo. O parâmetro linha não pode ser nulo.
+Semaforo semaforo_ler(const char *linha);
+
+void semaforo_escrever_informacoes(FILE *arquivo, Semaforo sema);
+
+void semaforo_escrever_svg(FILE *arquivo, Semaforo sema);
+
+// Retorna o id de um semaforo.
+const char *semaforo_obter_id(Semaforo sem);
+
+// Retorna a coordenada y de um semaforo.
+double semaforo_obter_y(Semaforo sem);
+// Retorna a coordenada x de um semaforo.
+double semaforo_obter_x(Semaforo sem);
+
+// Retorna a cor da borda de um semaforo
+const char *semaforo_obter_cor_borda(Semaforo sem);
+// Define a cor de borda de um semaforo.
+// O parâmetros cor_borda não pode ser nulo.
+void semaforo_definir_cor_borda(Semaforo sema, const char *cor_borda);
+
+// Retorna a cor de preenchimento do semaforo.
+// O semaforo passado como parâmetro deve existir.
+const char *semaforo_obter_cor_preenchimento(Semaforo sem);
+// Define a cor de preenchimento de um semaforo.
+// O parâmetros cor_preenchimento não pode ser nulo.
+void semaforo_definir_cor_preenchimento(Semaforo sema, const char *cor_preenchimento);
+
+// Retorna o valor da espessura da borda de um semaforo.
+int semaforo_obter_espessura_borda(Semaforo sem);
+// Define a espessura da borda de um semaforo.
+// O parâmetros espessura_borda não pode ser nulo.
+void semaforo_definir_espessura_borda(Semaforo sem, int espessura_borda);
+
+// Libera a memória alocada por um semáforo.
+void semaforo_destruir(Semaforo sema);
+
+#endif
