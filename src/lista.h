2efc4d2d975714c0e9930dd28337eace9abef736
Felipe Dallmann Tomazeli <56651954+felipedallmann@users.noreply.github.com>
2020-09-07 20:37:11 -0300

    Corrigir comentários do módulo lista
    Padroniza os comentários conforme o especificado.

--- src/lista.h
+++ src/lista.h
@@ -5,69 +5,95 @@
 
 typedef void* Lista;
 typedef void* No;
-
-//  Cria a cabeça da lista dinamica duplamente encadeada
-//  Sem requerimentos
-//  retorna o endereço para uma lista vazia
+/*
+Cria o ponteiro inicial de lista dinamica duplamente encadeada.
+Não é passado nenhum parâmetro.
+Retorna o endereço para uma lista vazia.
+*/
 Lista lista_create();
 
-//  Obtém o tamanho da lista (número de nós)
-//  É necessário passar a lista como parâmetro
-//  retorna um inteiro igual ao tamanho da lista
+/*
+Obtém o tamanho da lista (número de nós).
+O parâmetro lista não pode ser nulo e deve ser uma lista existente.
+retorna um inteiro igual ao tamanho da lista.
+*/
 int lista_get_length(Lista lista);
 
-//  insere um elemento no final da lista
-//  é preciso inicar a lista antes de inserir um elemento
-//  não retorna nada
+/*
+Insere um elemento no final da lista.
+Ambos os parâmetros não podem ser nulos, o parâmetro lista deve ser uma lista existente.
+Retrna o endereço para o elemento adicionado.
+*/
 No lista_insert_final(Lista lista, Figura figura);
 
-//  insere um elemento posterior um determinado p
-//  p deve ser um elemento válido da lista
-//  é retornado o endereço do elemento inserido
+/*
+Insere um elemento posterior a um determinado nó "p".
+Todos os parâmetros não podem ser nulos e o parâmetro p deve ser um elemento válido da lista.
+É retornado o endereço do elemento inserido.
+*/
 No lista_insert_before(Lista lista, Figura figura, No p);
 
-//  insere um elemento anterior um determinado p
-//  p deve ser um elemento válido da lista
-//  é retornado o endereço do elemento inserido
+/*
+Insere um elemento anterior a um determinado nó "p".
+Todos os parâmetros não podem ser nulos e o parâmetro p deve ser um elemento válido da lista.
+É retornado o endereço do elemento inserido.
+*/
 No lista_insert_after(Lista lista, Figura figura, No p);
 
-//  remove um elemento da lista
-//  o id passado tem que ser válido
-//  não retorna nada
+/*
+Remove um elemento da lista.
+Todos os parâmetros não podem ser nulos e o parâmetro lista deve ser uma lista existente.
+*/
 void lista_remove_no(Lista lista, No no_selecionado);
 
-//  utilizada para ter acesso a um nó da lista
-//  o id passado tem q ser válido
-//  retorna o endereço do nó
-No lista_get_no(Lista lista, const char id[100]);
+/*
+Acessa  um nó da lista.
+O id passado deve ser válido.
+Retorna o endereço do nó.
+*/
+No lista_get_no(Lista lista, char id[100]);
 
-//  pega o último elemento da lista
-//  a lista não pode estar vazia
-//  retorna o endereço do último nó
+/*
+Acessa o último elemento da lista.
+O parâmetro lista não pode ser nulo, deve ser uma lista existe e a lista não pode estar vazia.
+Retorna o endereço do último nó.
+*/
 No lista_get_last(Lista lista);
 
-//  pega o primeiro elemento da lista
-//  a lista não pode estar vazia
-//  retorna o endereço do primeiro nó
+/*
+Acessa o primeiro elemento da lista
+O parâmetro lista não pode ser nulo, deve ser uma lista existe e a lista não pode estar vazia.
+Retorna o endereço do primeiro nó.
+*/
 No lista_get_first(Lista lista);
 
-//  acessa as informações de um nó
-//  p não pode ser NULL e deve indicar um endereço válido que esteja na lista
-//  retorna o endereço para as informações do nó especificado por p
+/*
+Acessa as informações de um nó.
+O parâmetro p não pode ser NULL e deve indicar um endereço válido que esteja na lista.
+Retorna o endereço para as informações do nó especificado por p.
+*/
 Figura lista_get_figura(No p);
 
-//  acessa o proximo elemento de p
-//  p deve indicar um endereço válido que esteja na lista
-//  retorna o endereço do elemento posterior de p
+/*
+Acessa o elemento posterior ao elemento p.
+Ambos parâmetros não podem ser nulos e o parâmetro p deve indicar um endereço válido que esteja na
+lista.
+Retorna o endereço do elemento posterior de p.
+*/
 No lista_get_next(Lista lista, No p);
 
-//  acessa o proximo elemento de p
-//  p deve indicar um endereço válido que esteja na lista
-//  retorna o endereço do elemento anterior de p
+/*
+Acessa o elemento anterior ao elemento p.
+Ambos parâmetros não podem ser nulos e o parâmetro p deve indicar um endereço válido que esteja na
+lista.
+Retorna o endereço do elemento posterior de p.
+*/
 No lista_get_previous(Lista lista, No p);
 
-//  libera a memoria alocada em todos os elementos da lista
-//  é necessário que a lista seja incializada para que seja possível liberar
+/*
+Libera a memória alocada em todos os elementos da lista.
+O parâmetro lista não pode ser nulo e deve ser uma lista existente.
+*/
 void lista_libera_lista(Lista lista);
 
 #endif
