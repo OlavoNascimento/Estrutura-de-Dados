9f39f35bf437720d1d504535ec70cdced0a90760
Felipe Dallmann Tomazeli <56651954+felipedallmann@users.noreply.github.com>
2020-09-07 20:57:42 -0300

    Corrigir comentários do módulo rádio-base
    Padroniza os comentários conforme o especificado.

--- src/radio_base.h
+++ src/radio_base.h
@@ -6,50 +6,110 @@
 
 #include "circulo.h"
 
+/*
+Este módulo define o tipo Radio, assim como funções que criam, acessam suas propriedades e
+realizam operações entre variáveis deste tipo.
+*/
 typedef void* Radio;
 
-// Cria e inicializa uma struct RadioBase com os valores passados.
-// retorna o endereço para a struct criada
-Radio radio_criar(const char id[100], double raio, double x, double y, const char cor_borda[20],
-                  const char cor_preenchimento[20]);
+/*
+Cria e inicializa um struct Radio com os valores passados.
+O raio deve ser maior que 0. O id e as cores não podem ser nulos.
+Retorna um ponteiro para o struct. O usuário é responsável por liberar a memória alocada!
+*/
+Radio radio_criar(char id[100], double raio, double x, double y, char cor_borda[20],
+                  char cor_preenchimento[20]);
 
-// Cria e inicializa uma struct RadioBase(igual ao circulo) a partir de um texto obtido no arquivo
-// .geo. O parâmetro linha não pode ser nulo.
+/*
+Cria e inicializa um struct Radio a partir de um texto obtido no arquivo .geo.
+O parâmetro linha não pode ser nulo.
+Retorna um ponteiro para o struct. O usuário é responsável por liberar a memória alocada!
+*/
 Radio radio_ler(const char* linha);
 
+/*
+Escreve todas as informações presentes em uma radio-base em um arquivo.
+Nenhum dos parâmetros podem ser nulos. O arquivo deve estar aberto para escrita!
+*/
 void radio_escrever_informacoes(FILE* arquivo, Radio rad);
 
+/*
+Escreve o código svg necessário para representar uma radio-base em um arquivo.
+Nenhum dos parâmetros podem ser nulos. O arquivo deve estar aberto para escrita!
+*/
 void radio_escrever_svg(FILE* arquivo, Radio rad);
 
-// Retorna o id de um rádio base.
+/*
+Obtém o id de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna o id de uma rádio-base.
+*/
 const char* radio_obter_id(Radio rad);
 
-// Retorna o raio de um radio base. Usado apenas para cálculo interno, já que o radio base é
-// considerado um ponto.
+/*
+Obtém o raio de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna o raio de uma rádio-base.
+*/
 double radio_obter_raio(Radio rad);
-// Retorna a coordenada y de uma rádio-base.
+
+/*
+Obtém a coordenada y de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna a coordenada y de uma rádio-base.
+*/
 double radio_obter_y(Radio rad);
-// Define a coordenada x de uma rádio-base.
+
+/*
+Obtém a coordenada x de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna a coordenada x de uma rádio-base.
+*/
 double radio_obter_x(Radio rad);
 
-// Retorna a cor de preenchimento de uma rádio-base.
+/*
+Obtém a cor de preenchimento de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna a cor de preenchimento de uma rádio-base.
+*/
 const char* radio_obter_cor_preenchimento(Radio rad);
-// Define a cor de preenchimento de uma RadioBase.
-// O parâmetros cor_preenchimento não pode ser nulo.
+
+/*
+Define a cor de preenchimento de uma rádio-base.
+Nenhum dos parâmetros podem ser nulos.
+*/
 void radio_definir_cor_preenchimento(Radio rad, const char* cor_preenchimento);
 
-// Retorna a cor da borda de uma rádio-base.
+/*
+Obtém a cor da borda de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna a cor da borda de uma rádio-base.
+*/
 const char* radio_obter_cor_borda(Radio rad);
-// Define a cor de borda de um RadioBase.
-// O parâmetros cor_borda não pode ser nulo.
+
+/*
+Define a cor da borda de uma rádio-base.
+Nenhum dos parâmetros podem ser nulos.
+*/
 void radio_definir_cor_borda(Radio rad, const char* cor_borda);
 
-// Retorna a espessura da borda de um rádio base.
+/*
+Obtém a espessura da borda de uma rádio-base.
+O parâmetro rad não pode ser nulo.
+Retorna a espessura da borda de uma rádio-base.
+*/
 const char* radio_obter_espessura_borda(Radio rad);
-// Define a espessura da borda de um rádio base.
-void radio_definir_espessura_borda(Radio rad, const char* espessura_borda);
 
-// Libera a memória alocada por um radio base.
+/*
+Define a espessura da borda de um rádio-base.
+Nenhum dos parâmetros podem ser nulos.
+*/
+void radio_definir_espessura_borda(Radio rad, char* espessura_borda);
+
+/*
+Libera a memória alocada para um semáforo
+O parâmetro "sem" não pode ser nulo, deve ser um semáforo existente.
+*/
 void radio_destruir(Radio rad);
 
 #endif
