efe4ed5dc746af34d56b599eeab29eefe466c5d0
Olavo Nascimento <olavo.nascimento4@gmail.com>
2020-12-29 21:47:16 -0300

    Tornar Densidade compatível com uma figura genérica.
    Adiciona uma FiguraInterface a implementação da Densidade e registra as funções necessárias para ser utilizade como uma figura.
    Além disso a descrição do módulo foi refeita para especificar a finalidade do mesmo.

--- src/Objetos/Outros/densidade.c
+++ src/Objetos/Outros/densidade.c
@@ -5,16 +5,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "lista.h"
-#include "logging.h"
+#include "../../Estruturas/lista.h"
+#include "../../Utils/logging.h"
 
 typedef struct {
+    FiguraInterface vtable;
     double x;
     double y;
     double altura;
     double largura;
     double densidade;
 } DenImp;
+
+const char *densidade_obter_tipo() {
+    return "densidade";
+}
+
+// Conecta as funções do objeto Densidade com as da interface figura.
+static FiguraInterface densidade_criar_interface_figura() {
+    FiguraInterface interface = figura_interface_criar();
+    figura_registrar_obter_tipo(interface, densidade_obter_tipo);
+    figura_registrar_destruir(interface, densidade_destruir);
+    return interface;
+}
 
 Densidade densidade_criar(double x, double y, double largura, double altura, double densidade) {
     DenImp *denImp = malloc(sizeof(DenImp));
@@ -23,6 +36,8 @@ Densidade densidade_criar(double x, double y, double largura, double altura, dou
     denImp->x = x;
     denImp->y = y;
     denImp->densidade = densidade;
+
+    denImp->vtable = densidade_criar_interface_figura();
     return denImp;
 }
 
@@ -36,12 +51,13 @@ Densidade densidade_ler(const char *linha) {
     return densidade_criar(x, y, largura, altura, densidade);
 }
 
-// Retorna o número de habitantes.
+// Retorna o número de habitantes contidos na área da densidade.
 double densidade_calcular_habitantes(DenImp *regiao) {
     return regiao->densidade * (regiao->largura / 1000) * (regiao->altura / 1000);
 }
 
-bool checarArea(DenImp *regiao, double x, double y) {
+// Retorna true se um ponto é contido dentro da densidade.
+bool densidade_contem_ponto(DenImp *regiao, double x, double y) {
     if (x >= regiao->x && y >= regiao->y) {
         if (x <= (regiao->x + regiao->largura) && y <= (regiao->y + regiao->altura)) {
             return true;
@@ -50,15 +66,19 @@ bool checarArea(DenImp *regiao, double x, double y) {
     return false;
 }
 
+// Retorna o número de habitantes que vivem dentro de uma densidade.
 double densidade_buscar_coordenada(Lista lista_densidades, double x, double y) {
-    for (No i = lista_get_first(lista_densidades); i != NULL; i = lista_get_next(i)) {
-        DenImp *regiao = (DenImp *) figura_obter_figura(lista_get_figura(i));
-        if (checarArea(regiao, x, y))
+    for (No i = lista_obter_primeiro(lista_densidades); i != NULL; i = lista_obter_proximo(i)) {
+        DenImp *regiao = (DenImp *) lista_obter_figura(i);
+        if (densidade_contem_ponto(regiao, x, y))
             return densidade_calcular_habitantes(regiao);
     }
     return 0;
 }
 
-void densidade_destruir(Densidade dens) {
-    free(dens);
+// Libera a memória utilizada por uma densidade.
+void densidade_destruir(Densidade densidade) {
+    DenImp *denImp = (DenImp *) densidade;
+    free(denImp->vtable);
+    free(denImp);
 }
--- src/Objetos/Outros/densidade.h
+++ src/Objetos/Outros/densidade.h
@@ -1,38 +1,52 @@
 #ifndef DENSIDADE_H
 #define DENSIDADE_H
 
+/*
+Este módulo define uma Densidade, o qual representa uma área retangular que contem a densidade
+populacional em sua área. Possui o número de habitantes por km^2, assim como altura, largura e
+cordenadas espaciais.
+
+Suas propriedades são:
+    densidade: Número de habitantes por km^2 na área delimitado pela densidade.
+    largura
+    altura
+    x: Coordenada x da densidade no plano.
+    y: Coordenada y da densidade no plano.
+*/
+
 #include <stdbool.h>
 #include <stdio.h>
 
-#include "lista.h"
+#include "../../Estruturas/lista.h"
 
 typedef void *Densidade;
 
 /*
-Cria e inicializa um struct Densidade com os valores passados.
+Cria e inicializa um Densidade com os valores passados.
 A densidade não pode ser menor que zero.
-Retorna um ponteiro para o struct. O usuário é responsável por liberar a memória alocada!
+O usuário é responsável por liberar a memória alocada!
 */
 Densidade densidade_criar(double x, double y, double largura, double altura, double densidade);
 
 /*
-Cria e inicializa um struct Densidade a partir de um texto obtido no arquivo .geo.
+Cria e inicializa um Densidade a partir de um texto obtido no arquivo .geo.
 O parâmetro linha não pode ser nulo.
-Retorna um ponteiro para o struct. O usuário é responsável por liberar a memória alocada!
+O usuário é responsável por liberar a memória alocada!
 */
 Densidade densidade_ler(const char *linha);
 
 /*
-Verifica se um ponto está contido em uma densidade.
+Checa se um ponto está contido por as densidades armazenadas em uma lista e retorna o número de
+habitantes.
 O parâmetro lista_densidades não pode ser nulo.
-Retorna a densidade caso o ponto esteja contido.
+Retorna o número de habitantes que vivem dentro da densidade.
 */
 double densidade_buscar_coordenada(Lista lista_densidades, double x, double y);
 
 /*
 Libera a memória alocada por uma densidade.
 O parâmetro densidade não pode ser nulo e deve apontar para um espaço de memória reservada.
-Libera a memória alocada pelo struct. O ponteiro não poderá ser utilizado após isso!
+Libera a memória alocada. O ponteiro não poderá ser utilizado após isso!
 */
 void densidade_destruir(Densidade densidade);
 
