fbdeaf9bcb809fc862fff3d1c75bd510e43c5510
Olavo Nascimento <olavo.nascimento4@gmail.com>
2020-12-29 21:49:34 -0300

    Adicionar descrição para o módulo logging.

--- src/Utils/logging.h
+++ src/Utils/logging.h
@@ -1,12 +1,16 @@
 #ifndef DEBUG_SIGUEL_H
 #define DEBUG_SIGUEL_H
 
+/*
+Este módulo define macros auxiliares para logging da execução do programa.
+*/
+
 #ifdef DEBUG_SIGUEL
     #define LOG_INFO(fmt, ...) printf(fmt, ##__VA_ARGS__)
-    #define LOG_ERROR(fmt, ...) fprintf(stderr, "ERRO: " fmt, ##__VA_ARGS__)
+    #define LOG_ERRO(fmt, ...) fprintf(stderr, "ERRO: " fmt, ##__VA_ARGS__)
 #else
     #define LOG_INFO(fmt, ...)
-    #define LOG_ERROR(fmt, ...)
+    #define LOG_ERRO(fmt, ...)
 #endif
 
 #endif
