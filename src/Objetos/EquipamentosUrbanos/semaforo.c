2f96ceb41ca897341c643c76ab2e232630489038
Olavo Nascimento <olavo.nascimento4@gmail.com>
2020-12-29 21:45:50 -0300

    Tornar Semaforo compatível com uma figura genérica.
    Adiciona uma FiguraInterface a implementação do Semaforo e registra as funções necessárias para ser utilizado como uma figura.
    Além disso a descrição do módulo foi refeita para especificar a finalidade do mesmo.

--- src/Objetos/EquipamentosUrbanos/semaforo.c
+++ src/Objetos/EquipamentosUrbanos/semaforo.c
@@ -5,31 +5,103 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "logging.h"
-#include "retangulo.h"
-#include "texto.h"
+#include "../../Interfaces/figura.h"
+#include "../../Utils/logging.h"
+#include "../Formas/retangulo.h"
+#include "../Outros/texto.h"
 
 typedef struct {
-    Retangulo ret;
+    FiguraInterface vtable;
+    char id[100];
+    double largura;
+    double altura;
+    double x;
+    double y;
+    char cor_borda[20];
+    char cor_preenchimento[20];
+    double arredondamento_borda;
+    char espessura_borda[20];
+    bool borda_tracejada;
 } SemaforoImp;
+
+const char *semaforo_obter_tipo() {
+    return "semáforo";
+}
+
+// Escreve no svg as informações de uma radio base
+void semaforo_escrever_svg(Semaforo semaforo, FILE *arquivo) {
+    retangulo_escrever_svg(semaforo, arquivo);
+
+    double x = figura_obter_x_centro(semaforo);
+    double y = figura_obter_y_centro(semaforo) + 4;
+    // Rótulo do semáforo.
+    Texto texto_semaforo = texto_criar("", x, y, "none", "white", "S", true);
+    texto_escrever_svg(texto_semaforo, arquivo);
+    texto_destruir(texto_semaforo);
+}
+
+// Conecta as funções do objeto Semaforo com as da interface figura.
+// Como o struct SemaforoImp é idêntico ao struct RetanguloImp as funções utilizadas em um
+// objeto Retangulo podem ser reaproveitadas.
+static FiguraInterface semaforo_criar_interface_figura() {
+    FiguraInterface interface = figura_interface_criar();
+    figura_registrar_escrever_informacoes(interface, retangulo_escrever_informacoes);
+    figura_registrar_escrever_svg(interface, semaforo_escrever_svg);
+
+    figura_registrar_obter_tipo(interface, semaforo_obter_tipo);
+
+    figura_registrar_obter_id(interface, retangulo_obter_id);
+
+    figura_registrar_obter_x(interface, retangulo_obter_x);
+    figura_registrar_obter_y(interface, retangulo_obter_y);
+
+    figura_registrar_obter_x_inicio(interface, retangulo_obter_x);
+    figura_registrar_obter_y_inicio(interface, retangulo_obter_y);
+
+    figura_registrar_obter_x_fim(interface, retangulo_obter_x_fim);
+    figura_registrar_obter_y_fim(interface, retangulo_obter_y_fim);
+
+    figura_registrar_obter_x_centro(interface, retangulo_obter_x_centro);
+    figura_registrar_obter_y_centro(interface, retangulo_obter_y_centro);
+
+    figura_registrar_obter_cor_borda(interface, retangulo_obter_cor_borda);
+    figura_registrar_definir_cor_borda(interface, retangulo_definir_cor_borda);
+
+    figura_registrar_obter_cor_preenchimento(interface, retangulo_obter_cor_preenchimento);
+    figura_registrar_definir_cor_preenchimento(interface, retangulo_definir_cor_preenchimento);
+
+    figura_registrar_destruir(interface, retangulo_destruir);
+    return interface;
+}
 
 // Cria e inicializa um struct SemaforoImp com os valores passados.
 Semaforo semaforo_criar(const char id[100], double largura, double altura, double x, double y,
                         const char cor_borda[20], const char cor_preenchimento[20]) {
     if (id == NULL) {
-        LOG_ERROR("Não é possível criar um semaforo com id NULL!\n");
+        LOG_ERRO("Não é possível criar um semáforo com id NULL!\n");
         return NULL;
     }
     if (cor_borda == NULL) {
-        LOG_ERROR("Não é possível criar um semaforo com cor de borda NULL!\n");
+        LOG_ERRO("Não é possível criar um semáforo com cor de borda NULL!\n");
         return NULL;
     }
     if (cor_preenchimento == NULL) {
-        LOG_ERROR("Não é possível criar um semaforo com cor de preenchimento NULL!\n");
+        LOG_ERRO("Não é possível criar um semáforo com cor de preenchimento NULL!\n");
         return NULL;
     }
     SemaforoImp *semImp = malloc(sizeof(SemaforoImp));
-    semImp->ret = retangulo_criar(id, largura, altura, x, y, cor_borda, cor_preenchimento);
+    strcpy(semImp->id, id);
+    semImp->largura = largura;
+    semImp->altura = altura;
+    semImp->x = x;
+    semImp->y = y;
+    strcpy(semImp->cor_borda, cor_borda);
+    strcpy(semImp->cor_preenchimento, cor_preenchimento);
+    semImp->arredondamento_borda = 0;
+    semImp->borda_tracejada = false;
+    strcpy(semImp->espessura_borda, "1px");
+
+    semImp->vtable = semaforo_criar_interface_figura();
     return semImp;
 }
 
@@ -47,97 +119,67 @@ Semaforo semaforo_ler(const char *linha) {
                           cor_preenchimento);
 }
 
-// Escreve as informações de uma radio base
-void semaforo_escrever_informacoes(FILE *arquivo, Semaforo semaforo) {
-    SemaforoImp *semImp = (SemaforoImp *) semaforo;
-    retangulo_escrever_informacoes(arquivo, semImp->ret);
+// Retorna o id de um semáforo.
+const char *semaforo_obter_id(Semaforo semaforo) {
+    return retangulo_obter_id(semaforo);
 }
 
-// Escreve no svg as informações de uma radio base
-void semaforo_escrever_svg(FILE *arquivo, Semaforo semaforo) {
-    SemaforoImp *semImp = (SemaforoImp *) semaforo;
-    retangulo_escrever_svg(arquivo, semImp->ret);
-    double x = retangulo_obter_x(semImp->ret) + retangulo_obter_largura(semImp->ret) / 2;
-    double y = retangulo_obter_y(semImp->ret) + retangulo_obter_altura(semImp->ret) / 2 + 4;
-    Texto texto_semaforo = texto_criar("", x, y, "none", "white", "S", true);
-    texto_escrever_svg(arquivo, texto_semaforo);
-    texto_destruir(texto_semaforo);
+// Retorna a coordenada x de um semáforo.
+double semaforo_obter_x(Semaforo semaforo) {
+    return retangulo_obter_x(semaforo);
 }
 
-// Retorna o id de um semaforo.
-const char *semaforo_obter_id(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_id(semImp->ret);
+// Retorna a coordenada y de um semáforo.
+double semaforo_obter_y(Semaforo semaforo) {
+    return retangulo_obter_y(semaforo);
 }
 
-double semaforo_obter_x(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_x(semImp->ret);
+// Retorna a largura de um semáforo.
+double semaforo_obter_largura(Semaforo semaforo) {
+    return retangulo_obter_largura(semaforo);
 }
 
-double semaforo_obter_y(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_y(semImp->ret);
+// Retorna a altura de um semáforo.
+double semaforo_obter_altura(Semaforo semaforo) {
+    return retangulo_obter_altura(semaforo);
 }
 
-double semaforo_obter_largura(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_largura(semImp->ret);
-}
-
-double semaforo_obter_altura(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_altura(semImp->ret);
-}
-
-const char *semaforo_obter_cor_borda(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_cor_borda(semImp->ret);
+// Retorna a cor da borda de um semáforo.
+const char *semaforo_obter_cor_borda(Semaforo semaforo) {
+    return retangulo_obter_cor_borda(semaforo);
 }
 
 // Define a cor da borda de um semaforo.
 void semaforo_definir_cor_borda(Semaforo semaforo, const char *cor_borda) {
-    if (cor_borda == NULL) {
-        LOG_ERROR("Não é possível definir NULL como cor da borda de um semaforo!\n");
-        return;
-    }
-    SemaforoImp *semImp = (SemaforoImp *) semaforo;
-    retangulo_definir_cor_borda(semImp->ret, cor_borda);
+    retangulo_definir_cor_borda(semaforo, cor_borda);
 }
 
-const char *semaforo_obter_cor_preenchimento(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_cor_preenchimento(semImp->ret);
+// Retorna a cor de preenchimento de um semáforo.
+const char *semaforo_obter_cor_preenchimento(Semaforo semaforo) {
+    return retangulo_obter_cor_preenchimento(semaforo);
 }
 
 // Define a cor de preenchimento de um semaforo.
 void semaforo_definir_cor_preenchimento(Semaforo semaforo, const char *cor_preenchimento) {
-    if (cor_preenchimento == NULL) {
-        LOG_ERROR("Não é possível definir NULL como cor de preenchimento de um semaforo!\n");
-        return;
-    }
-    SemaforoImp *semImp = (SemaforoImp *) semaforo;
-    retangulo_definir_cor_preenchimento(semImp->ret, cor_preenchimento);
+    retangulo_definir_cor_preenchimento(semaforo, cor_preenchimento);
 }
 
-const char *semaforo_obter_espessura_borda(Semaforo sem) {
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    return retangulo_obter_espessura_borda(semImp->ret);
+// Define o arredondamento da borda de um semáforo.
+void semaforo_definir_espessura_borda(Semaforo semaforo, const char *espessura_borda) {
+    retangulo_definir_espessura_borda(semaforo, espessura_borda);
 }
 
-void semaforo_definir_espessura_borda(Semaforo sem, const char *espessura_borda) {
-    if (espessura_borda == NULL) {
-        LOG_ERROR(
-            "Não é possível definir NULL como tamanho da espessura da borda de um semaforo!\n");
-        return;
-    }
-    SemaforoImp *semImp = (SemaforoImp *) sem;
-    retangulo_definir_espessura_borda(semImp->ret, espessura_borda);
+// Define se a borda do semaforo é tracejada.
+void semaforo_definir_borda_tracejada(Semaforo semaforo, bool tracejado) {
+    retangulo_definir_borda_tracejada(semaforo, tracejado);
+}
+
+// Define o arredondamento da borda do semaforo.
+void semaforo_definir_arredondamento_borda(Semaforo semaforo, double arredondamento_borda) {
+    retangulo_definir_arredondamento_borda(semaforo, arredondamento_borda);
 }
 
 // Libera a memória alocada por um semáforo.
 void semaforo_destruir(Semaforo semaforo) {
-    SemaforoImp *semImp = (SemaforoImp *) semaforo;
-    retangulo_destruir(semImp->ret);
-    free(semaforo);
+    retangulo_destruir(semaforo);
 }
--- src/Objetos/EquipamentosUrbanos/semaforo.h
+++ src/Objetos/EquipamentosUrbanos/semaforo.h
@@ -1,43 +1,40 @@
 #ifndef SEMAFORO_H
 #define SEMAFORO_H
 
+/*
+Este módulo define um Semaforo, o qual representa um retângulo com cores e rótulo específico ao seu
+tipo para fácil diferenciação no arquivo svg.
+
+Suas propriedades são:
+    id: Identificador do semáforo.
+    largura
+    altura
+    x: Coordenada x do semáforo no plano.
+    y: Coordenada y do semáforo no plano.
+    cor_borda
+    cor_preenchimento
+    espessura_borda
+*/
+
 #include <stdbool.h>
 #include <stdio.h>
-
-#include "retangulo.h"
-
-/*
-Este módulo define o tipo Semaforo, assim como funções que criam e acessam suas propriedades.
-*/
 
 typedef void *Semaforo;
 
 /*
-Cria e inicializa um struct Semaforo a partir dos parâmetros passados pela função semaforo_ler.
+Cria e inicializa um Semaforo a partir dos parâmetros passados pela função semaforo_ler.
 A largura e altura devem ser maiores que 0. O id e as cores não podem ser nulos.
-Retorna o ponteiro para a struct criada. O usuário é responsável por liberar a memória alocada!
+Retorna o ponteiro. O usuário é responsável por liberar a memória alocada!
 */
 Semaforo semaforo_criar(const char id[100], double largura, double altura, double x, double y,
                         const char cor_borda[20], const char cor_preenchimento[20]);
 
 /*
-Cria e inicializa um struct Semaforo a partir de um texto obtido no arquivo .geo.
+Cria e inicializa um Semaforo a partir de um texto obtido no arquivo .geo.
 O parâmetro linha não pode ser nulo.
-Retorna o ponteiro para a struct criada. O usuário é responsável por liberar a memória alocada!
+Retorna o ponteiro. O usuário é responsável por liberar a memória alocada!
 */
 Semaforo semaforo_ler(const char *linha);
-
-/*
-Escreve todas as informações presentes de um semaforo em um arquivo.
-Nenhum dos parâmetros podem ser nulos. O arquivo deve estar aberto para escrita!
-*/
-void semaforo_escrever_informacoes(FILE *arquivo, Semaforo sema);
-
-/*
-Escreve o código svg necessário para representar um semaforo em um arquivo.
-Nenhum dos parâmetros podem ser nulos. O arquivo deve estar aberto para escrita!
-*/
-void semaforo_escrever_svg(FILE *arquivo, Semaforo sema);
 
 /*
 Obtém o id de um determinado semáforo.
@@ -47,18 +44,18 @@ Retorna o id do semáforo.
 const char *semaforo_obter_id(Semaforo semaforo);
 
 /*
-Obtém a coordenada y de um determinado semáforo.
-O parâmetro semaforo não pode ser nulo e deve ser um semáforo existente.
-Retorna a coordenada y do semáforo.
-*/
-double semaforo_obter_y(Semaforo semaforo);
-
-/*
 Obtém a coordenada x de um determinado semáforo.
 O parâmetro semaforo não pode ser nulo e deve ser um semáforo existente.
 Retorna a coordenada x do semáforo.
 */
 double semaforo_obter_x(Semaforo semaforo);
+
+/*
+Obtém a coordenada y de um determinado semáforo.
+O parâmetro semaforo não pode ser nulo e deve ser um semáforo existente.
+Retorna a coordenada y do semáforo.
+*/
+double semaforo_obter_y(Semaforo semaforo);
 
 /*
 Obtém a largura de um determinado semáforo.
@@ -86,7 +83,7 @@ Define a cor da borda de um determinado semáforo.
 O parâmetro semaforo não pode ser nulo, deve ser um semáforo existente e cor_borda não pode ser
 nulo.
 */
-void semaforo_definir_cor_borda(Semaforo sema, const char *cor_borda);
+void semaforo_definir_cor_borda(Semaforo semaforo, const char *cor_borda);
 
 /*
 Obtém a cor de preenchimento de um determinado semáforo.
@@ -100,14 +97,7 @@ Define a cor de preenchimento de um determinado semáforo.
 O parâmetro semaforo não pode ser nulo, deve ser um semáforo existente e cor_preenchimento não pode
 ser nulo.
 */
-void semaforo_definir_cor_preenchimento(Semaforo sema, const char *cor_preenchimento);
-
-/*
-Obtém a espessura de um determinado semáforo.
-O parâmetro semaforo não pode ser nulo e deve ser um semáforo existente.
-Retorna a espessura do semáforo.
-*/
-const char *semaforo_obter_espessura_borda(Semaforo semaforo);
+void semaforo_definir_cor_preenchimento(Semaforo semaforo, const char *cor_preenchimento);
 
 /*
 Define a espessura de um determinado semáforo.
@@ -117,9 +107,21 @@ ser nulo.
 void semaforo_definir_espessura_borda(Semaforo semaforo, const char *espessura_borda);
 
 /*
+Define se o semaforo tem borda tracejada.
+O parâmetro semaforo não pode ser nulo.
+*/
+void semaforo_definir_borda_tracejada(Semaforo semaforo, bool tracejado);
+
+/*
+Define o arredondamento da borda de um semaforo.
+O parâmetro semaforo não pode ser nulo.
+*/
+void semaforo_definir_arredondamento_borda(Semaforo semaforo, double raio_borda);
+
+/*
 Libera a memória alocada por um semaforo.
 O parâmetro semaforo não pode ser nulo e deve apontar para um espaço de memória reservada.
-Libera a memória alocada pelo struct. O ponteiro não poderá ser utilizado após isso!
+Libera a memória alocada. O ponteiro não poderá ser utilizado após isso!
 */
 void semaforo_destruir(Semaforo semaforo);
 
