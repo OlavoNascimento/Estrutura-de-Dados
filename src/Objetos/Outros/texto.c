0d956ebdafd259114d11b8fcbb67b68f0aadebe5
Olavo Nascimento <olavo.nascimento4@gmail.com>
2020-12-29 21:47:26 -0300

    Tornar Texto compatível com uma figura genérica.
    Adiciona uma FiguraInterface a implementação do Texto e registra as funções necessárias para ser utilizado como uma figura.
    Além disso a descrição do módulo foi refeita para especificar a finalidade do mesmo.

--- src/Objetos/Outros/texto.c
+++ src/Objetos/Outros/texto.c
@@ -5,9 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "logging.h"
+#include "../../Interfaces/figura.h"
+#include "../../Utils/logging.h"
+
+#define ALTURA_LETRA 2
+#define LARGURA_LETRA 4
 
 typedef struct {
+    FiguraInterface vtable;
     char id[100];
     double x;
     double y;
@@ -16,6 +21,42 @@ typedef struct {
     char conteudo[500];
     bool centralizar;
 } TextoImp;
+
+const char *texto_obter_tipo() {
+    return "texto";
+}
+
+// Conecta as funções do objeto texto com as da interface figura.
+static FiguraInterface texto_criar_interface_figura() {
+    FiguraInterface interface = figura_interface_criar();
+    figura_registrar_escrever_informacoes(interface, texto_escrever_informacoes);
+    figura_registrar_escrever_svg(interface, texto_escrever_svg);
+
+    figura_registrar_obter_tipo(interface, texto_obter_tipo);
+
+    figura_registrar_obter_id(interface, texto_obter_id);
+
+    figura_registrar_obter_x(interface, texto_obter_x);
+    figura_registrar_obter_y(interface, texto_obter_y);
+
+    figura_registrar_obter_x_inicio(interface, texto_obter_x);
+    figura_registrar_obter_y_inicio(interface, texto_obter_y);
+
+    figura_registrar_obter_x_fim(interface, texto_obter_x_fim);
+    figura_registrar_obter_y_fim(interface, texto_obter_y_fim);
+
+    figura_registrar_obter_x_centro(interface, texto_obter_x_centro);
+    figura_registrar_obter_y_centro(interface, texto_obter_y_centro);
+
+    figura_registrar_obter_cor_borda(interface, texto_obter_cor_borda);
+    figura_registrar_definir_cor_borda(interface, texto_definir_cor_borda);
+
+    figura_registrar_obter_cor_preenchimento(interface, texto_obter_cor_preenchimento);
+    figura_registrar_definir_cor_preenchimento(interface, texto_definir_cor_preenchimento);
+
+    figura_registrar_destruir(interface, texto_destruir);
+    return interface;
+}
 
 // Cria e inicializa um struct Texto com os valores passados.
 Texto texto_criar(const char id[100], double x, double y, const char cor_borda[20],
@@ -28,25 +69,28 @@ Texto texto_criar(const char id[100], double x, double y, const char cor_borda[2
     strcpy(texImp->cor_preenchimento, cor_preenchimento);
     strcpy(texImp->conteudo, conteudo);
     texImp->centralizar = centralizar;
+
+    texImp->vtable = texto_criar_interface_figura();
     return texImp;
 }
 
-// Cria um texto com base em informações de uma linha.
-Texto texto_ler(const char *linha) {
+// Cria um texto com base em informações de uma texto.
+Texto texto_ler(const char *texto) {
     char id[100];
     double x;
     double y;
     char cor_borda[20];
     char cor_preenchimento[20];
     char conteudo[500];
-    sscanf(linha, "%*s %s %lf %lf %s %s %[^\n*]s", id, &x, &y, cor_borda, cor_preenchimento,
+    sscanf(texto, "%*s %s %lf %lf %s %s %[^\n*]s", id, &x, &y, cor_borda, cor_preenchimento,
            conteudo);
     return texto_criar(id, x, y, cor_borda, cor_preenchimento, conteudo, false);
 }
 
 // Escreve todos os dados de um texto em um arquivo.
-void texto_escrever_informacoes(FILE *arquivo, Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
+void texto_escrever_informacoes(Texto texto, FILE *arquivo) {
+    TextoImp *texImp = (TextoImp *) texto;
+    fprintf(arquivo, "tipo: %s, ", figura_obter_tipo(texto));
     if (strlen(texImp->id) > 0)
         fprintf(arquivo, "id: %s, ", texImp->id);
     fprintf(arquivo, "x: %lf, y: %lf, corb: %s, corp: %s, texto: %s\n", texImp->x, texImp->y,
@@ -54,8 +98,8 @@ void texto_escrever_informacoes(FILE *arquivo, Texto tex) {
 }
 
 // Escreve o código svg que representa um texto em um arquivo.
-void texto_escrever_svg(FILE *arquivo, Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
+void texto_escrever_svg(Texto texto, FILE *arquivo) {
+    TextoImp *texImp = (TextoImp *) texto;
     fprintf(arquivo, "\t<text");
     if (strlen(texImp->id) > 0)
         fprintf(arquivo, " id='%s'", texImp->id);
@@ -65,82 +109,87 @@ void texto_escrever_svg(FILE *arquivo, Texto tex) {
             texImp->cor_borda, texImp->cor_preenchimento, texImp->conteudo);
 }
 
-const char *texto_obter_id(Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
-    return texImp->id;
-}
-
-void texto_definir_id(Texto tex, const char *id) {
-    if (id == NULL) {
-        LOG_ERROR("Não é possível definir NULL como id de um texto!\n");
-        return;
-    }
-    TextoImp *texImp = (TextoImp *) tex;
-    strcpy(texImp->id, id);
-}
-
-double texto_obter_x(Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
-    return texImp->x;
-}
-
-void texto_definir_x(Texto tex, double x) {
-    TextoImp *texImp = (TextoImp *) tex;
-    texImp->x = x;
-}
-
-double texto_obter_y(Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
-    return texImp->y;
-}
-
-void texto_definir_y(Texto tex, double y) {
-    TextoImp *texImp = (TextoImp *) tex;
-    texImp->y = y;
-}
-
-const char *texto_obter_cor_borda(Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
-    return texImp->cor_borda;
-}
-
-void texto_definir_cor_borda(Texto tex, const char *cor_borda) {
-    if (cor_borda == NULL) {
-        LOG_ERROR("Não é possível definir NULL como cor da borda de um texto!\n");
-        return;
-    }
-    TextoImp *texImp = (TextoImp *) tex;
-    strcpy(texImp->cor_borda, cor_borda);
-}
-
-const char *texto_obter_cor_preenchimento(Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
-    return texImp->cor_preenchimento;
-}
-
-void texto_definir_cor_preenchimento(Texto tex, const char *cor_preenchimento) {
-    if (cor_preenchimento == NULL) {
-        LOG_ERROR("Não é possível definir NULL como cor de preenchimento de um texto!\n");
-        return;
-    }
-    TextoImp *texImp = (TextoImp *) tex;
-    strcpy(texImp->cor_preenchimento, cor_preenchimento);
-}
-
-const char *texto_obter_conteudo(Texto tex) {
-    TextoImp *texImp = (TextoImp *) tex;
+// Obtém o conteúdo de um texto.
+const char *texto_obter_conteudo(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
     return texImp->conteudo;
 }
 
-void texto_definir_conteudo(Texto tex, const char *conteudo) {
-    if (conteudo == NULL) {
-        LOG_ERROR("Não é possível definir NULL como conteúdo de um texto!\n");
-        return;
-    }
-    TextoImp *texImp = (TextoImp *) tex;
-    strcpy(texImp->conteudo, conteudo);
+// Retorna o id de um texto.
+const char *texto_obter_id(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->id;
 }
 
-void texto_destruir(Texto tex) {
-    free(tex);
+// Retorna a coordenada x de um texto.
+double texto_obter_x(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->x;
+}
+
+// Retorna a coordenada y de um texto.
+double texto_obter_y(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->y;
+}
+
+// Retorna um valor utilizando uma largura estimada.
+double texto_obter_x_fim(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->x + strlen(texImp->conteudo) * LARGURA_LETRA;
+}
+
+// Retorna um valor utilizando uma altura estimada.
+double texto_obter_y_fim(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->y + ALTURA_LETRA;
+}
+
+// Retorna a coordenada x do centro de um texto.
+double texto_obter_x_centro(Texto texto) {
+    return (texto_obter_x_fim(texto) - texto_obter_x(texto)) / 2;
+}
+
+// Retorna a coordenada y do centro de um texto.
+double texto_obter_y_centro(Texto texto) {
+    return (texto_obter_y_fim(texto) - texto_obter_y(texto)) / 2;
+}
+
+// Retorna a cor da borda de um texto.
+const char *texto_obter_cor_borda(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->cor_borda;
+}
+
+// Define a cor da borda de um texto.
+void texto_definir_cor_borda(Texto texto, const char *cor_borda) {
+    if (cor_borda == NULL) {
+        LOG_ERRO("Não é possível definir NULL como cor da borda de um texto!\n");
+        return;
+    }
+    TextoImp *texImp = (TextoImp *) texto;
+    strcpy(texImp->cor_borda, cor_borda);
+}
+
+// Retorna a cor de preenchimento de um texto.
+const char *texto_obter_cor_preenchimento(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    return texImp->cor_preenchimento;
+}
+
+// Define a cor de preenchimento de um texto.
+void texto_definir_cor_preenchimento(Texto texto, const char *cor_preenchimento) {
+    if (cor_preenchimento == NULL) {
+        LOG_ERRO("Não é possível definir NULL como cor de preenchimento de um texto!\n");
+        return;
+    }
+    TextoImp *texImp = (TextoImp *) texto;
+    strcpy(texImp->cor_preenchimento, cor_preenchimento);
+}
+
+// Libera a memória alocada por um texto.
+void texto_destruir(Texto texto) {
+    TextoImp *texImp = (TextoImp *) texto;
+    free(texImp->vtable);
+    free(texImp);
 }
--- src/Objetos/Outros/texto.h
+++ src/Objetos/Outros/texto.h
@@ -1,27 +1,37 @@
 #ifndef TEXTO_H
 #define TEXTO_H
 
+/*
+Este módulo define um Texto, permitindo a exibição de textos no arquivo svg. Possui como
+propriedades seu identificador, coordenadas espacias e para alterar sua visualização no arquivo svg
+cores de borda e preenchimento e caso necessário é possível centralizar seu conteudo.
+
+Suas propriedades são:
+    id: Identificador do texto.
+    x: Coordenada x do texto no plano.
+    y: Coordenada y do texto no plano.
+    cor_borda
+    cor_preenchimento
+    conteudo
+*/
+
 #include <stdbool.h>
 #include <stdio.h>
-
-/*
-Este módulo define o tipo Texto, assim como funções que criam e acessam suas propriedades.
-*/
 
 typedef void *Texto;
 
 /*
-Cria e inicializa uma struct Texto com os valores passados.
+Cria e inicializa uma Texto com os valores passados.
 A largura e altura devem ser maiores que 0. O id e as cores não podem ser nulos.
-Retorna um ponteiro para o struct. O usuário é responsável por liberar a memória alocada!
+O usuário é responsável por liberar a memória alocada!
 */
 Texto texto_criar(const char id[100], double x, double y, const char cor_borda[20],
                   const char cor_preenchimento[20], const char conteudo[500], bool centralizar);
 
 /*
-Cria e inicializa uma struct Texto a partir de um texto obtido no arquivo .geo.
+Cria e inicializa uma Texto a partir de um texto obtido no arquivo .geo.
 O parâmetro linha não pode ser nulo.
-Retorna um ponteiro para o struct. O usuário é responsável por liberar a memória alocada!
+O usuário é responsável por liberar a memória alocada!
 */
 Texto texto_ler(const char *linha);
 
@@ -29,91 +39,101 @@ Texto texto_ler(const char *linha);
 Escreve todas as informações presentes em um texto em um arquivo.
 Nenhum dos parâmetros podem ser nulos. O arquivo deve estar aberto para escrita!
 */
-void texto_escrever_informacoes(FILE *arquivo, Texto tex);
+void texto_escrever_informacoes(Texto texto, FILE *arquivo);
 
 /*
 Escreve todas as informações presentes em um texto em um arquivo.
 Nenhum dos parâmetros podem ser nulos. O arquivo deve estar aberto para escrita!
 */
-void texto_escrever_svg(FILE *arquivo, Texto tex);
-
-/*
-Obtém o id de um texto.
-O parâmetro retangulo não pode ser nulo.
-Retorna o id de um texto.
-*/
-const char *texto_obter_id(Texto tex);
-/*
-Define o id de um texto.
-Nenhum dos parâmetros podem ser nulos.
-*/
-void texto_definir_id(Texto tex, const char *id);
-
-/*
-Obtém a coordenada x de um texto.
-O parâmetro retangulo não pode ser nulo.
-Retorna a coordenada x de um texto.
-*/
-double texto_obter_x(Texto tex);
-/*
-Define a coordenada x de um texto.
-O parâmetro retangulo não pode ser nulo.
-*/
-void texto_definir_x(Texto tex, double x);
-
-/*
-Obtém a coordenada y de um texto.
-O parâmetro retangulo não pode ser nulo.
-Retorna a coordenada y de um texto.
-*/
-double texto_obter_y(Texto tex);
-/*
-Define a coordenada y de um texto.
-O parâmetro retangulo não pode ser nulo.
-*/
-void texto_definir_y(Texto tex, double y);
-
-/*
-Obtém a cor da borda de um texto.
-O parâmetro retangulo não pode ser nulo.
-Retorna a cor da borda de um texto.
-*/
-const char *texto_obter_cor_borda(Texto tex);
-/*
-Define a cor da borda de um texto.
-Nenhum dos parâmetros podem ser nulos.
-*/
-void texto_definir_cor_borda(Texto tex, const char *cor_borda);
-
-/*
-Obtém a cor de preenchimento de um texto.
-O parâmetro retangulo não pode ser nulo.
-Retorna a cor de preenchimento de um texto.
-*/
-const char *texto_obter_cor_preenchimento(Texto tex);
-/*
-Define a cor de preenchimento de um texto.
-Nenhum dos parâmetros podem ser nulos.
-*/
-void texto_definir_cor_preenchimento(Texto tex, const char *cor_preenchimento);
+void texto_escrever_svg(Texto texto, FILE *arquivo);
 
 /*
 Obtém o conteúdo de um texto.
 O parâmetro retangulo não pode ser nulo.
 Retorna o conteúdo de um texto.
 */
-const char *texto_obter_conteudo(Texto tex);
+const char *texto_obter_conteudo(Texto texto);
+
 /*
-Define o conteúdo de um texto.
+Obtém o id de um texto.
+O parâmetro retangulo não pode ser nulo.
+Retorna o id de um texto.
+*/
+const char *texto_obter_id(Texto texto);
+
+/*
+Obtém a coordenada x de um texto.
+O parâmetro retangulo não pode ser nulo.
+Retorna a coordenada x de um texto.
+*/
+double texto_obter_x(Texto texto);
+
+/*
+Obtém a coordenada y de um texto.
+O parâmetro retangulo não pode ser nulo.
+Retorna a coordenada y de um texto.
+*/
+double texto_obter_y(Texto texto);
+
+/*
+Obtém a coordenada x onde um texto termina.
+O parâmetro retangulo não pode ser nulo.
+Retorna a coordenada x onde um texto termina.
+*/
+double texto_obter_x_fim(Texto texto);
+
+/*
+Obtém a coordenada y onde um texto termina.
+O parâmetro retangulo não pode ser nulo.
+Retorna a coordenada y onde um texto termina.
+*/
+double texto_obter_y_fim(Texto texto);
+
+/*
+Obtém a coordenada x onde o centro de um texto se encontra.
+O parâmetro retangulo não pode ser nulo.
+Retorna a coordenada x onde o centro de um texto se encontra.
+*/
+double texto_obter_x_centro(Texto texto);
+
+/*
+Obtém a coordenada y onde o centro de um texto se encontra.
+O parâmetro retangulo não pode ser nulo.
+Retorna a coordenada y onde o centro de um texto se encontra.
+*/
+double texto_obter_y_centro(Texto texto);
+
+/*
+Obtém a cor da borda de um texto.
+O parâmetro retangulo não pode ser nulo.
+Retorna a cor da borda de um texto.
+*/
+const char *texto_obter_cor_borda(Texto texto);
+
+/*
+Define a cor da borda de um texto.
 Nenhum dos parâmetros podem ser nulos.
 */
-void texto_definir_conteudo(Texto tex, const char *conteudo);
+void texto_definir_cor_borda(Texto texto, const char *cor_borda);
+
+/*
+Obtém a cor de preenchimento de um texto.
+O parâmetro retangulo não pode ser nulo.
+Retorna a cor de preenchimento de um texto.
+*/
+const char *texto_obter_cor_preenchimento(Texto texto);
+
+/*
+Define a cor de preenchimento de um texto.
+Nenhum dos parâmetros podem ser nulos.
+*/
+void texto_definir_cor_preenchimento(Texto texto, const char *cor_preenchimento);
 
 /*
 Libera a memória alocada por um texto.
 O parâmetro retangulo não pode ser nulo e deve apontar para um espaço de memória reservada.
-Libera a memória alocada pelo struct. O ponteiro não poderá ser utilizado após isso!
+Libera a memória alocada. O ponteiro não poderá ser utilizado após isso!
 */
-void texto_destruir(Texto tex);
+void texto_destruir(Texto texto);
 
 #endif
