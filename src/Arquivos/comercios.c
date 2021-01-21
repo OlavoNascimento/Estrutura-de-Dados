3df2c68867bade7a9576a57cb664eef085b97955
Olavo Nascimento <olavo.nascimento4@gmail.com>
2021-01-20 22:12:49 -0300

    Adicionar comando t ao arquivo de comércios.
    Adiciona uma função capaz de executar o comando t específico ao arquivo de comércios (.ec). Através dessa função um código referente ao tipo de um estabelecimento é relacionado a uma descrição e adicionado a um Hashmap.

--- src/Arquivos/comercios.c
+++ src/Arquivos/comercios.c
@@ -10,6 +10,15 @@
 // Tamanho maxímo de um comando do arquivo de comércios.
 #define TAMANHO_COMANDO 300
 
+// Lê um tipo de estabelecimento e a descrição desse tipo. Um par chave/valor é criado no hashmap
+// passado a função.
+void definir_descricao_tipo(Tabela descricoes, const char *linha) {
+    char tipo[100];
+    char descricao[100];
+    sscanf(linha, "t %s %s", tipo, descricao);
+    tabela_inserir(descricoes, tipo, descricao);
+}
+
 // Lê um arquivo de comércios fornecido a função e adiciona as figuras descritas em suas linha
 // como elementos de uma lista.
 void comercios_ler(const char *caminho_comercios, Tabela quadtrees, Tabela relacoes) {
@@ -22,15 +31,13 @@ void comercios_ler(const char *caminho_comercios, Tabela quadtrees, Tabela relac
     QuadTree moradores = tabela_buscar(quadtrees, "moradores");
     Tabela tipo_descricao = tabela_buscar(relacoes, "tipo_descricao");
 
-    char tipo_estabelecimentos[100];
-    tipo_estabelecimentos[0] = '\0';
-
     char linha[TAMANHO_COMANDO];
     while (fgets(linha, TAMANHO_COMANDO, arquivo_comercios) != NULL) {
         char comando[TAMANHO_COMANDO];
         sscanf(linha, "%s", comando);
 
         if (strcmp("t", comando) == 0) {
+            definir_descricao_tipo(tipo_descricao, linha);
         } else if (strcmp("e", comando) == 0) {
         }
     }
