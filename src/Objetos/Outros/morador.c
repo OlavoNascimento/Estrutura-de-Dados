6a556594559e03039c06ddef5d0105ba054c3879
Felipe Dallmann Tomazeli <56651954+felipedallmann@users.noreply.github.com>
2021-01-23 15:31:15 -0300

    Tornar morador compatível com uma figura genérica.
    Adiciona uma FiguraInterface a implementação do Morador e registra as funções necessárias para ser utilizado como uma figura. São utilizadas funções do módulo Retângulo.

--- src/Objetos/Outros/morador.c
+++ src/Objetos/Outros/morador.c
@@ -5,21 +5,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../../Estruturas/lista.h"
-#include "../../Estruturas/quadtree.h"
-#include "../../Estruturas/tabelahash.h"
+#include "../../Interfaces/figura.h"
 #include "../../Utils/logging.h"
+#include "../EquipamentosUrbanos/quadra.h"
+#include "../Formas/retangulo.h"
+#include "../Outros/texto.h"
 #include "morador.h"
 
 typedef struct {
-    // TODO adicionar interface
+    FiguraInterface vtable;
     char cpf[100];
+    double largura;
+    double altura;
+    double x;
+    double y;
+    char cor_borda[20];
+    char cor_preenchimento[20];
+    double arredondamento_borda;
+    char espessura_borda[20];
+    bool borda_tracejada;
+
     char nome[100];
     char sobrenome[100];
     char sexo;
     int dia;
     int mes;
     int ano;
+    char data[20];
 
     // endereço
     char cep[100];
@@ -28,91 +40,46 @@ typedef struct {
     char complemento[100];
 } MoradorImp;
 
-const char *morador_obter_tipo() {
+typedef struct {
+    double largura;
+    double altura;
+    double x;
+    double y;
+    char cor_borda[20];
+    char cor_preenchimento[20];
+    double arredondamento_borda;
+    char espessura_borda[20];
+    bool borda_tracejada;
+} Coordenadas;
+
+const char *morador_obter_string_tipo() {
     return "morador";
 }
 
-// Cria e inicializa um struct MoradorImp com os valores passados.
-Morador morador_criar(const char cpf[100], const char nome[100], const char sobrenome[100],
-                      const char sexo, int dia, int mes, int ano) {
-    if (cpf == NULL) {
-        LOG_ERRO("Não é possível criar um morador com cpf NULL!\n");
-        return NULL;
-    }
-    if (nome == NULL) {
-        LOG_ERRO("Não é possível criar um morador com nome NULL!\n");
-        return NULL;
-    }
-    if (sobrenome == NULL) {
-        LOG_ERRO("Não é possível criar um morador com sobrenome NULL!\n");
-        return NULL;
-    }
-    MoradorImp *moradorImp = malloc(sizeof *moradorImp);
-    stpcpy(moradorImp->cpf, cpf);
-    stpcpy(moradorImp->nome, nome);
-    stpcpy(moradorImp->sobrenome, sobrenome);
-    stpcpy(moradorImp->sexo, sexo);
-    moradorImp->dia = dia;
-    moradorImp->mes = mes;
-    moradorImp->ano = ano;
+// Escreve no svg as informações de um morador.
+void morador_escrever_svg(Morador morador, FILE *arquivo) {
+    retangulo_escrever_svg(morador, arquivo);
 
-    return moradorImp;
+    double x = figura_obter_x_centro(morador);
+    double y = figura_obter_y_centro(morador) + 6;
+    // Rótulo do morador.
+    Texto texto_morador = texto_criar("", x, y, "none", "white", "E", true);
+    texto_escrever_svg(texto_morador, arquivo);
+    texto_destruir(texto_morador);
 }
 
-// Cria um morador com base em informações de uma linha.
-Morador morador_ler(const char *linha) {
-    char cpf[100];
-    char nome[100];
-    char sobrenome[100];
-    char sexo;
-    int dia;
-    int mes;
-    int ano;
-    sscanf(linha, "%*s %s %s %s %c %d %*c %d %*c %d", cpf, nome, sobrenome, &sexo, &dia, &mes,
-           &ano);
-    return morador_criar(cpf, nome, sobrenome, sexo, dia, mes, ano);
-}
-
-void morador_ler_endereco(Morador morador, const char *linha) {
-    char cep[100];
-    char complemento[100];
-    char face;
-    int num;
-
-    sscanf(linha, "%*c %*s %s %c %d %s", cep, face, num, complemento);
-    morador_definir_endereco(morador, cep, face, num, complemento);
-}
-
-void morador_definir_endereco(Morador morador, const char *cep, const char face, int num,
-                              const char *complemento) {
+// Escreve todos os dados de uma morador em um arquivo.
+void morador_escrever_informacoes(Morador morador, FILE *arquivo) {
     MoradorImp *moradorImp = morador;
-    stpcpy(moradorImp->cep, cep);
-    stpcpy(moradorImp->complemento, complemento);
-    moradorImp->face = face;
-    moradorImp->num = num;
+    fprintf(arquivo,
+            "tipo: %s cpf: %s, nome: %s, sobrenome: %s, sexo: %c, data: %s, x: %lf y: %lf, corb: "
+            "%s, corp: %s, ",
+            figura_obter_tipo(moradorImp), moradorImp->cpf, moradorImp->nome, moradorImp->sobrenome,
+            moradorImp->sexo, morador_obter_data(moradorImp), moradorImp->x, moradorImp->y,
+            moradorImp->cor_borda, moradorImp->cor_preenchimento);
 }
 
-const char *morador_obter_id(Morador morador) {
-    MoradorImp *moradorImp = morador;
-    return moradorImp->cep;
-}
-
-const char *morador_obter_nome(Morador morador) {
-    MoradorImp *moradorImp = morador;
-    return moradorImp->nome;
-}
-
-const char *morador_obter_sobrenome(Morador morador) {
-    MoradorImp *moradorImp = morador;
-    return moradorImp->sobrenome;
-}
-
-const char morador_obter_sexo(Morador morador) {
-    MoradorImp *moradorImp = morador;
-    return moradorImp->sexo;
-}
-
-const char *morador_obter_data(Morador morador) {
+void morador_definir_data(Morador morador) {
     MoradorImp *moradorImp = morador;
     char buffer[20];
     if (moradorImp->dia < 10) {
@@ -130,25 +97,218 @@ const char *morador_obter_data(Morador morador) {
         snprintf(buffer, sizeof(buffer), "%d/%d/%d", moradorImp->dia, moradorImp->mes,
                  moradorImp->ano);
     }
-    return buffer;
+
+    strcpy(moradorImp->data, buffer);
+}
+
+static FiguraInterface morador_criar_interface_figura() {
+    FiguraInterface interface = figura_interface_criar();
+    figura_registrar_escrever_informacoes(interface, morador_escrever_informacoes);
+    figura_registrar_escrever_svg(interface, morador_escrever_svg);
+
+    figura_registrar_obter_id(interface, retangulo_obter_id);
+
+    figura_registrar_obter_tipo(interface, morador_obter_string_tipo);
+
+    figura_registrar_obter_x(interface, retangulo_obter_x);
+    figura_registrar_obter_y(interface, retangulo_obter_y);
+
+    figura_registrar_obter_x_inicio(interface, retangulo_obter_x);
+    figura_registrar_obter_y_inicio(interface, retangulo_obter_y);
+
+    figura_registrar_obter_x_fim(interface, retangulo_obter_x_fim);
+    figura_registrar_obter_y_fim(interface, retangulo_obter_y_fim);
+
+    figura_registrar_obter_x_centro(interface, retangulo_obter_x_centro);
+    figura_registrar_obter_y_centro(interface, retangulo_obter_y_centro);
+
+    figura_registrar_obter_cor_borda(interface, retangulo_obter_cor_borda);
+    figura_registrar_definir_cor_borda(interface, retangulo_definir_cor_borda);
+
+    figura_registrar_obter_cor_preenchimento(interface, retangulo_obter_cor_preenchimento);
+    figura_registrar_definir_cor_preenchimento(interface, retangulo_definir_cor_preenchimento);
+
+    figura_registrar_destruir(interface, morador_destruir);
+    return interface;
+}
+
+// Cria e inicializa um struct MoradorImp com os valores passados.
+Morador morador_criar(const char *cpf, const char *nome, const char *sobrenome, const char sexo,
+                      int dia, int mes, int ano) {
+    if (cpf == NULL) {
+        LOG_ERRO("Não é possível criar um morador com cpf NULL!\n");
+        return NULL;
+    }
+    if (nome == NULL) {
+        LOG_ERRO("Não é possível criar um morador com nome NULL!\n");
+        return NULL;
+    }
+    if (sobrenome == NULL) {
+        LOG_ERRO("Não é possível criar um morador com sobrenome NULL!\n");
+        return NULL;
+    }
+    MoradorImp *moradorImp = malloc(sizeof *moradorImp);
+    strcpy(moradorImp->cpf, cpf);
+    strcpy(moradorImp->nome, nome);
+    strcpy(moradorImp->sobrenome, sobrenome);
+    moradorImp->sexo = sexo;
+    moradorImp->dia = dia;
+    moradorImp->mes = mes;
+    moradorImp->ano = ano;
+
+    morador_definir_data(moradorImp);
+
+    strcpy(moradorImp->cep, "não registrado");
+    strcpy(moradorImp->complemento, "-");
+    moradorImp->num = 0;
+    moradorImp->face = '-';
+
+    moradorImp->largura = 0;
+    moradorImp->altura = 0;
+    moradorImp->x = 0;
+    moradorImp->y = 0;
+    strcpy(moradorImp->cor_borda, "#3a464a");
+    strcpy(moradorImp->cor_preenchimento, "#2d3b40");
+    moradorImp->arredondamento_borda = 0;
+    moradorImp->borda_tracejada = false;
+    strcpy(moradorImp->espessura_borda, "1px");
+
+    moradorImp->vtable = morador_criar_interface_figura();
+    return moradorImp;
+}
+
+// Cria um morador com base em informações de uma linha.
+Morador morador_ler(const char *linha) {
+    char cpf[100];
+    char nome[100];
+    char sobrenome[100];
+    char sexo;
+    int dia;
+    int mes;
+    int ano;
+    sscanf(linha, "%*s %s %s %s %c %d %*c %d %*c %d", cpf, nome, sobrenome, &sexo, &dia, &mes,
+           &ano);
+    return morador_criar(cpf, nome, sobrenome, sexo, dia, mes, ano);
+}
+
+// Obtém o endereço de um morador com base em informações de uma linha.
+void morador_ler_endereco(Morador morador, const char *linha, Quadra quadra) {
+    char cep[100];
+    char complemento[100];
+    char face;
+    int num;
+
+    sscanf(linha, "m %*s %s %c %d %s", cep, &face, &num, complemento);
+    morador_definir_endereco(morador, cep, face, num, complemento, quadra);
+}
+
+void morador_definir_endereco(Morador morador, const char *cep, const char face, int num,
+                              const char *complemento, Quadra quadra) {
+    MoradorImp *moradorImp = morador;
+    strcpy(moradorImp->cep, cep);
+    strcpy(moradorImp->complemento, complemento);
+    moradorImp->face = face;
+    moradorImp->num = num;
+
+    const double largura = 12;
+    const double altura = 12;
+    double x = 0;
+    double y = 0;
+    quadra_inicializar_coordenada(&x, &y, largura, altura, quadra, face, num);
+
+    moradorImp->largura = largura;
+    moradorImp->altura = altura;
+    moradorImp->x = x;
+    moradorImp->y = y;
+}
+
+const char *morador_obter_id(Morador morador) {
+    return retangulo_obter_id(morador);
+}
+
+const char *morador_obter_nome(Morador morador) {
+    MoradorImp *moradorImp = morador;
+    return moradorImp->nome;
+}
+
+const char *morador_obter_sobrenome(Morador morador) {
+    MoradorImp *moradorImp = morador;
+    return moradorImp->sobrenome;
+}
+
+char morador_obter_sexo(Morador morador) {
+    MoradorImp *moradorImp = morador;
+    return moradorImp->sexo;
+}
+
+const char *morador_obter_data(Morador morador) {
+    MoradorImp *moradorImp = morador;
+    return moradorImp->data;
 }
 
 const char *morador_obter_endereco_cep(Morador morador) {
     MoradorImp *moradorImp = morador;
     return moradorImp->cep;
-    char buffer[100];
 }
 
-const char morador_obter_endereco_face(Morador morador) {
+char morador_obter_endereco_face(Morador morador) {
     MoradorImp *moradorImp = morador;
     return moradorImp->face;
-    char buffer[100];
 }
 
 int morador_obter_endereco_num(Morador morador) {
     MoradorImp *moradorImp = morador;
     return moradorImp->num;
-    char buffer[100];
+}
+
+// Retorna a coordenada x de um morador.
+double morador_obter_x(Morador morador) {
+    return retangulo_obter_x(morador);
+}
+
+// Retorna a coordenada y de um morador.
+double morador_obter_y(Morador morador) {
+    return retangulo_obter_y(morador);
+}
+
+// Retorna a largura de um morador.
+double morador_obter_largura(Morador morador) {
+    return retangulo_obter_largura(morador);
+}
+
+// Retorna a altura de um morador.
+double morador_obter_altura(Morador morador) {
+    return retangulo_obter_altura(morador);
+}
+
+// Retorna a cor da borda de um morador.
+const char *morador_obter_cor_borda(Morador morador) {
+    return retangulo_obter_cor_borda(morador);
+}
+
+// Define a cor da borda de um morador.
+void morador_definir_cor_borda(Morador morador, const char *cor_borda) {
+    retangulo_definir_cor_borda(morador, cor_borda);
+}
+
+// Retorna a cor de preenchimento de um morador.
+const char *morador_obter_cor_preenchimento(Morador morador) {
+    return retangulo_obter_cor_preenchimento(morador);
+}
+
+// Define a cor de preenchimento de um morador.
+void morador_definir_cor_preenchimento(Morador morador, const char *cor_preenchimento) {
+    retangulo_definir_cor_preenchimento(morador, cor_preenchimento);
+}
+
+// Define a espessura da borda de um morador.
+void morador_definir_espessura_borda(Morador morador, const char *espessura_borda) {
+    retangulo_definir_espessura_borda(morador, espessura_borda);
+}
+
+// Define o arredondamento da borda de um morador.
+void morador_definir_arredondamento_borda(Morador morador, double raio_borda) {
+    retangulo_definir_arredondamento_borda(morador, raio_borda);
 }
 
 // Libera a memória utilizada por um morador.
--- src/Objetos/Outros/morador.h
+++ src/Objetos/Outros/morador.h
@@ -1,18 +1,50 @@
 #ifndef MORADOR_H
 #define MORADOR_H
 
+/*
+Este módulo define um Morador, o qual representa um retângulo com informações adicionais
+como cpf, nome, sobrenome e data de nascimento
+ ademais possui cores e rótulo específico ao seu tipo para fácil
+diferenciação no arquivo svg.
+
+Suas propriedades são:
+    id: cpf do morador
+    largura
+    altura
+    x: Coordenada x do morador no plano.
+    y: Coordenada y do morador no plano.
+    cor_borda
+    cor_preenchimento
+    arredondamento_borda
+    espessura_borda
+    tipo: tipo de morador.
+    nome: nome do morador.
+    sobrenome: sobrenome do morador.
+    dia/mes/ano: data de nascimento do morador
+*/
+
 #include <stdbool.h>
 #include <stdio.h>
+
+#include "../EquipamentosUrbanos/quadra.h"
 
 typedef void *Morador;
 
 /*
 Cria e inicializa um Morador com os valores passados.
-O cpf, nome e sobrenomem não podem ser nulos.
+Argumentos:
+    cpf: Identificador do morador sendo criado.
+    nome: Nome do morador.
+    sobrenome: Sobrenome do morador.
+    sexo: M sendo másculino ou F sendo feminino.
+    dia: Dia de nascimento do morador.
+    mes: Mês de nascimento do morador.
+    ano: Ano de nascimento do morador.
+cpf, nome e sobrenome nao podem ser nulos
 O usuário é responsável por liberar a memória alocada!
 */
-Morador morador_criar(const char cpf[100], const char nome[100], const char sobrenome[100],
-                      const char sexo, int dia, int mes, int ano);
+Morador morador_criar(const char *cpf, const char *nome, const char *sobrenome, const char sexo,
+                      int dia, int mes, int ano);
 
 /*
 Cria e inicializa um Morador a partir de um texto obtido no arquivo .pm.
@@ -25,14 +57,16 @@ Morador morador_ler(const char *linha);
 Define o endereço de um morador a partir de um texto obtido no arquivo.pm.
 Nenhum dos parâmetros pode ser nulo.
 */
-void morador_ler_endereco(Morador morador, const char *linha);
+void morador_ler_endereco(Morador morador, const char *linha, Quadra quadra);
 
 /*
 Define o endereço de um morador com os valores passados.
+O morador é inserido em uma quadtree assim que as suas
+coordenadas são obtidas através do endereço.
 O morador e cep não podem ser nulos.
 */
 void morador_definir_endereco(Morador morador, const char *cep, const char face, int num,
-                              const char *complemento);
+                              const char *complemento, Quadra quadra);
 
 /*
 Obtém o id(cpf) de um morador.
@@ -60,7 +94,7 @@ Obtém o sexo de um morador.
 O parâmetro morador não pode ser nulo.
 Retorna o sexo de um morador.
 */
-const char morador_obter_sexo(Morador morador);
+char morador_obter_sexo(Morador morador);
 
 /*
 Obtém a data de nascimento de um morador.
@@ -81,7 +115,7 @@ Obtém a face do endereço de um morador.
 O parâmetro morador não pode ser nulo.
 Retorna a face do endereço de um morador.
 */
-const char morador_obter_endereco_face(Morador morador);
+char morador_obter_endereco_face(Morador morador);
 
 /*
 Obtém o número do endereço de um morador.
@@ -89,6 +123,79 @@ O parâmetro morador não pode ser nulo.
 Retorna o número do endereço de um morador.
 */
 int morador_obter_endereco_num(Morador morador);
+
+// /////////////////////////
+/*
+Obtém a coordenada x de um morador.
+O parâmetro morador não pode ser nulo.
+Retorna a coordenada x de um morador.
+*/
+double morador_obter_x(Morador morador);
+
+/*
+Obtém a coordenada y de um morador.
+O parâmetro morador não pode ser nulo.
+Retorna o raio de um morador.
+*/
+double morador_obter_y(Morador morador);
+
+/*
+Obtém a largura de um morador.
+O parâmetro morador não pode ser nulo.
+Retorna a largura de um morador.
+*/
+double morador_obter_largura(Morador morador);
+
+/*
+Obtém a altura de um morador.
+O parâmetro morador não pode ser nulo.
+Retorna a altura de um morador.
+*/
+double morador_obter_altura(Morador morador);
+
+/*
+Obtém a cor de preenchimento de um morador.
+O parâmetro morador não pode ser nulo.
+Retorna a cor de preenchimento de um morador.
+*/
+const char *morador_obter_cor_preenchimento(Morador morador);
+
+/*
+Define a cor de preenchimento de um morador.
+Nenhum dos parâmetros podem ser nulos.
+*/
+void morador_definir_cor_preenchimento(Morador morador, const char *cor_preenchimento);
+
+/*
+Obtém a cor da borda de um morador.
+O parâmetro morador não pode ser nulo.
+Retorna a cor da borda de um morador.
+*/
+const char *morador_obter_cor_borda(Morador morador);
+
+/*
+Define a cor da borda de um morador.
+Nenhum dos parâmetros podem ser nulos.
+*/
+void morador_definir_cor_borda(Morador morador, const char *cor_borda);
+
+/*
+Define a espessura da borda de um morador.
+Nenhum dos parâmetros podem ser nulos.
+*/
+void morador_definir_espessura_borda(Morador morador, const char *espessura_borda);
+
+/*
+Define se o morador tem borda tracejada.
+O parâmetro morador não pode ser nulo.
+*/
+void morador_definir_borda_tracejada(Morador morador, bool tracejado);
+
+/*
+Define o arredondamento da borda de um morador.
+O parâmetro morador não pode ser nulo.
+*/
+void morador_definir_arredondamento_borda(Morador morador, double raio_borda);
 
 /*
 Libera a memória alocada por um morador.
