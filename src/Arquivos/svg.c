15f97fe08608a3f5ba96d17db37b468b6729c15a
Olavo Nascimento <olavo.nascimento4@gmail.com>
2021-02-08 10:58:44 -0300

    Permitir escrever svg de listas.
    Reintroduz uma função capaz de transformar figuras armazenadas em listas em um arquivo svg correspondente.

--- src/Arquivos/svg.c
+++ src/Arquivos/svg.c
@@ -13,38 +13,42 @@
 // Margem entre as bordas do svg e as figuras.
 #define SVG_MARGEM 14
 
+typedef void AplicarInfo(void *info, void *extra);
+
 // Utilizado para representar as proporções de um arquivo svg.
 typedef struct {
     double origem_x;
     double origem_y;
     double largura;
     double altura;
-} ExibicaoSVG;
+} ProporcoesSVG;
 
 // Checa se é necessário atualizar os valores da struct ExibiçãoSVG para garantir que a figura
 // passada a função possa ser vista no arquivo svg.
-void svg_atualizar_exibicao(Figura figura, ExibicaoSVG *exi) {
+void svg_atualizar_proporcao(Figura figura, ProporcoesSVG *exi) {
     exi->origem_x = min(exi->origem_x, figura_obter_x_inicio(figura));
     exi->origem_y = min(exi->origem_y, figura_obter_y_inicio(figura));
     exi->largura = max(exi->largura, figura_obter_x_fim(figura));
     exi->altura = max(exi->altura, figura_obter_y_fim(figura));
 }
 
-// Retorna uma struct que representa as propriedades iniciais do arquivo svg.
-ExibicaoSVG *svg_criar_exibicao(int num_quadtrees, va_list quadtrees) {
-    va_list exi_qts;
-    va_copy(exi_qts, quadtrees);
+// Retorna uma struct que representa as proporcoes de um arquivo svg. Para isso é preciso iterar por
+// todas as figuras de uma estrutura, atualizando a struct conforme necessário.
+ProporcoesSVG *svg_criar_proporcao(int num_estruturas, va_list estruturas,
+                                   void estrutura_map(void *estrutura, visitaNo f, void *extra)) {
+    va_list copia_estruturas;
+    va_copy(copia_estruturas, estruturas);
 
-    ExibicaoSVG *exi = malloc(sizeof *exi);
+    ProporcoesSVG *exi = malloc(sizeof *exi);
     exi->origem_x = DBL_MAX;
     exi->origem_y = DBL_MAX;
     exi->largura = DBL_MIN;
     exi->altura = DBL_MIN;
 
     // Encontra os valores necessários para que todas as figuras apareçam no arquivo svg.
-    for (int i = 0; i < num_quadtrees; i++) {
-        QuadTree qt = va_arg(exi_qts, QuadTree);
-        percorreLarguraQt(qt, (void *) svg_atualizar_exibicao, exi);
+    for (int i = 0; i < num_estruturas; i++) {
+        void *estrutura = va_arg(copia_estruturas, void *);
+        estrutura_map(estrutura, (void *) svg_atualizar_proporcao, exi);
     }
 
     exi->origem_x -= SVG_MARGEM;
@@ -83,39 +87,59 @@ void escrever_definicoes_sombras(FILE *arquivo) {
             "\t</defs>\n");
 }
 
-// Transforma as figuras das quadtrees em um código svg que as representam, salvando o resultado em
-// um arquivo svg localizado no caminho específicado.
-void svg_quadtrees_para_svg(const char *caminho_svg, int num_quadtrees, ...) {
-    if (caminho_svg == NULL) {
-        LOG_ERRO("Caminho nulo passado a lista para svg!\n");
-        return;
-    }
+// Escreve um arquivo svg utilizando as figuras armazenadas nas estruturas fornecidas.
+void escrever_arquivo_svg(const char *caminho_svg, int num_estruturas, va_list estruturas,
+                          void estrutura_map(void *estrutura, AplicarInfo f, void *extra)) {
     FILE *arquivo_svg = fopen(caminho_svg, "w");
     if (arquivo_svg == NULL) {
         fprintf(stderr, "ERRO: Arquivo svg %s não pode ser criado para escrita!\n", caminho_svg);
         return;
     }
 
-    va_list quadtrees;
-    va_start(quadtrees, num_quadtrees);
-
     // Calcula as proporções da imagem svg.
-    ExibicaoSVG *exibicao = svg_criar_exibicao(num_quadtrees, quadtrees);
+    ProporcoesSVG *proporcoes = svg_criar_proporcao(num_estruturas, estruturas, estrutura_map);
 
     fprintf(arquivo_svg, "<svg viewBox='%lf %lf %lf %lf' xmlns='http://www.w3.org/2000/svg'>\n",
-            exibicao->origem_x, exibicao->origem_y, exibicao->largura, exibicao->altura);
+            proporcoes->origem_x, proporcoes->origem_y, proporcoes->largura, proporcoes->altura);
 
     escrever_definicoes_sombras(arquivo_svg);
 
-    // Itera pelas quadtrees e escreve suas figuras.
-    for (int i = 0; i < num_quadtrees; i++) {
-        // Recebe a quadtree atual.
-        QuadTree qt = va_arg(quadtrees, QuadTree);
-        percorreLarguraQt(qt, (void *) figura_escrever_svg, arquivo_svg);
+    // Itera pelas estruturas e escreve suas figuras.
+    for (int i = 0; i < num_estruturas; i++) {
+        void *estrutura = va_arg(estruturas, void *);
+        estrutura_map(estrutura, (void *) figura_escrever_svg, arquivo_svg);
     }
+
     fprintf(arquivo_svg, "</svg>\n");
 
-    va_end(quadtrees);
+    free(proporcoes);
     fclose(arquivo_svg);
-    free(exibicao);
+}
+
+// Transforma as figuras das listas em um código svg que as representam, salvando o resultado
+// em um arquivo svg localizado no caminho especificado.
+void svg_escrever_listas(const char *caminho_svg, int num_listas, ...) {
+    if (caminho_svg == NULL) {
+        LOG_ERRO("Caminho nulo passado para svg_escrever_listas!\n");
+        return;
+    }
+
+    va_list listas;
+    va_start(listas, num_listas);
+    escrever_arquivo_svg(caminho_svg, num_listas, listas, lista_map);
+    va_end(listas);
+}
+
+// Transforma as figuras das quadtrees em um código svg que as representam, salvando o resultado
+// em um arquivo svg localizado no caminho especificado.
+void svg_escrever_quadtrees(const char *caminho_svg, int num_quadtrees, ...) {
+    if (caminho_svg == NULL) {
+        LOG_ERRO("Caminho nulo passado para svg_escrever_quadtrees!\n");
+        return;
+    }
+
+    va_list quadtrees;
+    va_start(quadtrees, num_quadtrees);
+    escrever_arquivo_svg(caminho_svg, num_quadtrees, quadtrees, percorreLarguraQt);
+    va_end(quadtrees);
 }
--- src/Arquivos/svg.h
+++ src/Arquivos/svg.h
@@ -2,9 +2,17 @@
 #define SVG_H
 
 /*
-Este módulo define a função svg_lista_para_svg, responsável por transformar todas as figuras em
-várias listas em um arquivo svg.
+Este módulo define a funções que convertem uma estrutura de dados para seu arquivo svg
+correspondente.
 */
+
+/*
+Escreve o svg correspondente a todas as listas passadas como argumento.
+O argumento caminho_svg não pode ser nulo, assim como as listas passadas. O parâmetro
+num_listas deve ser igual a quantidade de listas passadas a função.
+Cria um arquivo svg no caminho especificado.
+*/
+void svg_escrever_listas(const char *caminho_svg, int num_listas, ...);
 
 /*
 Escreve o svg correspondente a todas as quadtrees passadas como argumento.
@@ -12,6 +20,6 @@ O argumento caminho_svg não pode ser nulo, assim como as quadtrees passadas. O 
 num_quadtrees deve ser igual a quantidade de quadtrees passadas a função.
 Cria um arquivo svg no caminho especificado.
 */
-void svg_quadtrees_para_svg(const char *caminho_svg, int num_quadtrees, ...);
+void svg_escrever_quadtrees(const char *caminho_svg, int num_quadtrees, ...);
 
 #endif
