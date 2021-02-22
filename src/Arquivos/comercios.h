69e2d14b89e223ee9553c6409ea85fa3fa9f3712
Felipe Dallmann Tomazeli <56651954+felipedallmann@users.noreply.github.com>
2021-02-21 22:50:08 -0300

    Correção de comentários
    Correção de comentários no módulo "comercios.h".

--- src/Arquivos/comercios.h
+++ src/Arquivos/comercios.h
@@ -2,15 +2,15 @@
 #define COMERCIOS_H
 
 /*
-Este módulo define a função descricao_ler, responsável por interpretar um arquivo de descrição
-fornecido ao programa e executar os comandos especificados.
+Este módulo define a função comercios_ler, responsável por interpretar um arquivo de descrição de
+um comércio fornecido ao programa e executar os comandos especificados.
 */
 
 #include "../Estruturas/tabelahash.h"
 
 /*
 Abre um arquivo de estabelecimentos comerciasis (.ec) no caminho passado a função e interpreta seus
-comandos afim de popular quadtrees com os estabelecimentos especificados.
+comandos a fim de popular quadtrees com os estabelecimentos especificados.
 Nenhum dos parâmetros podem ser nulos e o caminho do arquivo de descrição deve ser válido.
 As quadtrees são preenchidas com os estabelcimentos descritos no arquivo.
 */
