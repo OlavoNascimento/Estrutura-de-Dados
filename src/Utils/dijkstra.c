695f5ac9dcab2ed818e6f994c9bda0fd92b7644d
Olavo Nascimento <olavo.nascimento4@gmail.com>
2021-06-05 22:23:46 -0300

    Simplificar algoritmo de Dijkstra.

--- src/Utils/dijkstra.c
+++ src/Utils/dijkstra.c
@@ -1,230 +1,123 @@
 #include <float.h>
 #include <stdbool.h>
 #include <stdlib.h>
-#include <string.h>
 
 #include "../Estruturas/grafo.h"
 #include "../Estruturas/lista.h"
 #include "../Estruturas/pilha.h"
-#include "../Estruturas/tabelahash.h"
 #include "../Utils/logging.h"
 
 typedef struct {
-    double distancia;
-    double velocidade;
+    double custo;
     int predecessor;
     bool aberto;
-} dijkstra_infos;
+} DijkstraInfos;
 
-void inicializaDijkstra(Grafo *grafo, dijkstra_infos *infos, int origem) {
-    for (int i = 0; i < grafo_obter_tamanho_atual(grafo); i++) {
+typedef double ObterValorComparado(Aresta aresta);
+
+DijkstraInfos *inicializa_dijkstra(Grafo grafo, int origem) {
+    // Os indices do arranjo DijkstraInfos serão os mesmos indices dos vertices do grafo
+    DijkstraInfos *infos = malloc(sizeof *infos * grafo_obter_tamanho(grafo));
+    if (infos == NULL) {
+        LOG_ERRO("Não foi possível alocar memória para as informações do dijkstra\n");
+        return NULL;
+    }
+    for (int i = 0; i < grafo_obter_tamanho(grafo); i++) {
         infos[i].aberto = true;
-        infos[i].distancia =
-            DBL_MAX /
-            2;  // dividido por 2 para não ter o risco de somar algum numero e passar o limite
+        // Dividido por 2 para não ter o risco de somar algum número e passar o limite.
+        infos[i].custo = DBL_MAX / 2;
         infos[i].predecessor = -1;
     }
-    infos[origem].distancia = 0;
+    infos[origem].custo = 0;
+    return infos;
 }
 
-bool existeAberto(Grafo *grafo, dijkstra_infos *infos) {
-    for (int i = 0; i < grafo_obter_tamanho_atual(grafo); i++) {
-        if (infos[i].aberto) {
+bool existe_aberto(Grafo grafo, DijkstraInfos *infos) {
+    for (int i = 0; i < grafo_obter_tamanho(grafo); i++) {
+        if (infos[i].aberto)
             return true;
-        }
     }
     return false;
 }
 
-int menorDistancia(Grafo *grafo, dijkstra_infos *infos) {
-    int i;
-    for (int i = 0; i < grafo_obter_tamanho_atual(grafo); i++) {
-        if (infos[i].aberto) {
+int menor_custo(Grafo grafo, DijkstraInfos *infos) {
+    int i = 0;
+    for (i = 0; i < grafo_obter_tamanho(grafo); i++) {
+        if (infos[i].aberto)
             break;
-        }
     }
-    if (i == grafo_obter_tamanho_atual(grafo)) {
-        // rodou todo o grafo e não encontrou vertices abertos
+    // Rodou todo o grafo e não encontrou vertices abertos
+    if (i == grafo_obter_tamanho(grafo))
         return -1;
-    }
 
     int menor = i;
-
-    for (i = menor + 1; i < grafo_obter_tamanho_atual(grafo); i++) {
-        if (infos[i].aberto && infos[menor].distancia > infos[i].distancia) {
+    for (i = menor + 1; i < grafo_obter_tamanho(grafo); i++) {
+        if (infos[i].aberto && infos[menor].custo > infos[i].custo)
             menor = i;
-        }
     }
     return menor;
 }
 
-int menorVelocidade(Grafo *grafo, dijkstra_infos *infos) {
-    int i;
-    for (i = 0; i < grafo_obter_tamanho_atual(grafo); i++) {
-        if (infos[i].aberto) {
-            break;
-        }
-    }
-    if (i == grafo_obter_tamanho_atual(grafo)) {
-        // rodou todo o grafo e não encontrou vertices abertos
-        return -1;
-    }
-
-    int menor = i;
-
-    for (i = menor + 1; i < grafo_obter_tamanho_atual(grafo); i++) {
-        if (infos[i].aberto && infos[menor].velocidade > infos[i].velocidade) {
-            menor = i;
-        }
-    }
-    return menor;
-}
-
-// relaxa todos os vertices adjacentes, definindo as distancias
-void relaxa_distancia(Grafo grafo, dijkstra_infos *infos, Tabela id_indice,
-                      Vertice vertice_origem_atual) {
-    const int *indice_origem = tabela_buscar(id_indice, vertice_obter_id(vertice_origem_atual));
-
+// Relaxa todos os vértices adjacentes, definindo o custo para cada vértice adjacente.
+void relaxa(Grafo grafo, DijkstraInfos *infos, Vertice vertice, ObterValorComparado obter_valor) {
+    const int *indice_origem = grafo_obter_indice_vertice(grafo, vertice_obter_id(vertice));
     if (indice_origem == NULL) {
         LOG_AVISO("Não é possível relaxar vértices adjacentes a uma origem inválida\n");
+        return;
     }
 
-    Lista arestas = vertice_obter_arestas(vertice_origem_atual);
-    if (arestas == NULL) {
-        LOG_AVISO(
-            "Não é possível relaxar vértices adjacentes ao vértice de origem com arestas "
-            "inválidas\n");
-    }
-
-    char id_destino[100];
-    double comprimento;
-    Aresta aresta_aux;
+    Lista arestas = vertice_obter_arestas(vertice);
     for_each_lista(aresta_no, arestas) {
-        aresta_aux = lista_obter_info(aresta_no);
-        comprimento = aresta_obter_comprimento(aresta_aux);
-        strcpy(id_destino, aresta_obter_destino(aresta_aux));
-        const int *indice_aux = tabela_buscar(id_indice, id_destino);
+        const Aresta aresta = lista_obter_info(aresta_no);
+        const double custo = obter_valor(aresta);
 
-        if (indice_aux == NULL) {
-            LOG_AVISO("Não é possível relaxar vértice inválido \n");
+        const char *id_destino = aresta_obter_destino(aresta);
+        const int *indice = grafo_obter_indice_vertice(grafo, id_destino);
+        if (indice == NULL) {
+            LOG_AVISO("Não é possível relaxar um vértice inválido\n");
+            return;
         }
 
-        // analisa se a distancia atualmente setada no indice do dikstra_infos, que representa o
-        // vertice de mesmo indice no grafo, é maior que a distancia obtida pelo caminho anterior
-        if (infos[*indice_aux].distancia > infos[*indice_origem].distancia + comprimento) {
-            infos[*indice_aux].distancia = infos[*indice_origem].distancia + comprimento;
-            infos[*indice_aux].predecessor = *indice_origem;
-        }
-    }
-}
-
-// relaxa todos os vertices adjacentes, definindo as distancias
-void relaxa_velocidade(Grafo grafo, dijkstra_infos *infos, Tabela id_indice,
-                       Vertice vertice_origem_atual) {
-    const int *indice_origem = tabela_buscar(id_indice, vertice_obter_id(vertice_origem_atual));
-
-    if (indice_origem == NULL) {
-        LOG_AVISO("Não é possível relaxar vértices adjacentes a uma origem inválida\n");
-    }
-
-    Lista arestas = vertice_obter_arestas(vertice_origem_atual);
-    if (arestas == NULL) {
-        LOG_AVISO(
-            "Não é possível relaxar vértices adjacentes ao vértice de origem com arestas "
-            "inválidas\n");
-    }
-
-    char id_destino[100];
-    double velocidade;
-    Aresta aresta_aux;
-    for_each_lista(aresta_no, arestas) {
-        aresta_aux = lista_obter_info(aresta_no);
-        velocidade = aresta_obter_velocidade(aresta_aux);
-        strcpy(id_destino, aresta_obter_destino(aresta_aux));
-        const int *indice_aux = tabela_buscar(id_indice, id_destino);
-
-        if (indice_aux == NULL) {
-            LOG_AVISO("Não é possível relaxar vértice inválido \n");
-        }
-
-        // analisa se a distancia atualmente setada no indice do dikstra_infos, que representa o
-        // vertice de mesmo indice no grafo, é maior que a distancia obtida pelo caminho anterior
-        if (infos[*indice_aux].distancia > infos[*indice_origem].distancia + velocidade) {
-            infos[*indice_aux].distancia = infos[*indice_origem].distancia + velocidade;
-            infos[*indice_aux].predecessor = *indice_origem;
+        // Analisa se o custo atualmente setado no indice do DijkstraInfos, que representa o
+        // vertice de mesmo indice no grafo, é maior que o custo obtido pelo caminho anterior.
+        if (infos[*indice].custo > infos[*indice_origem].custo + custo) {
+            infos[*indice].custo = infos[*indice_origem].custo + custo;
+            infos[*indice].predecessor = *indice_origem;
         }
     }
 }
 
 // Retorna uma pilha com os indices que definem o caminho da origem até o destino
-Pilha dijkstra_distancia(Grafo *grafo, int origem, int destino) {
-    // Os indices do arranjo dijkstra_infos serão os mesmos indices dos vertices do grafo
-    dijkstra_infos *infos = malloc(sizeof(dijkstra_infos) * grafo_obter_tamanho(grafo));
-    if (infos == NULL) {
-        LOG_ERRO("Não foi possivel alocar memória para informações do dijkstra\n");
-    }
+Pilha dijkstra(Grafo grafo, int origem, int destino, ObterValorComparado obter_valor_aresta) {
+    // Os indices do arranjo DijkstraInfos serão os mesmos indices dos vertices do grafo
+    DijkstraInfos *infos = inicializa_dijkstra(grafo, origem);
 
-    inicializaDijkstra(grafo, infos, origem);
-    Vertice vertice;
-
-    while (existeAberto(grafo, infos)) {
-        int origem_atual = menorDistancia(grafo, infos);
+    while (existe_aberto(grafo, infos)) {
+        int origem_atual = menor_custo(grafo, infos);
         infos[origem_atual].aberto = false;
 
-        vertice = grafo_obter_vertice_por_indice(grafo, origem_atual);
-
-        relaxa_distancia(grafo, infos, grafo_obter_tabela(grafo), vertice);
+        const Vertice vertice = grafo_obter_vertice_por_indice(grafo, origem_atual);
+        relaxa(grafo, infos, vertice, obter_valor_aresta);
     }
 
-    Pilha pilha_caminho = pilha_criar(free);
-
+    Pilha pilha_caminho = pilha_criar(NULL);
     if (pilha_caminho == NULL) {
-        LOG_ERRO("Não foi possivel alocar memória para pilha\n");
+        LOG_ERRO("Não foi possível alocar memória para pilha\n");
+        return NULL;
     }
 
-    int diferenca_destino_origem = abs(destino - origem);
-    int aux = destino;
-    for (int i = 0; i < diferenca_destino_origem; i++) {
-        if (aux == origem) {
-            break;
-        }
-        pilha_inserir(pilha_caminho, aux);
-        aux = infos[aux].predecessor;
-    }
+    // Adiciona os indices do menor caminho a pilha.
+    for (int i = destino; i != -1; i = infos[i].predecessor)
+        pilha_inserir(pilha_caminho, grafo_obter_vertice_por_indice(grafo, i));
+
+    free(infos);
+    return pilha_caminho;
 }
 
-Pilha dijkstra_velocidade(Grafo *grafo, int origem, int destino) {
-    // Os indices do arranjo dijkstra_infos serão os mesmos indices dos vertices do grafo
-    dijkstra_infos *infos = malloc(sizeof(dijkstra_infos) * grafo_obter_tamanho(grafo));
-    if (infos == NULL) {
-        LOG_ERRO("Não foi possivel alocar memória para informações do dijkstra\n");
-    }
+Pilha dijkstra_distancia(Grafo grafo, int origem, int destino) {
+    return dijkstra(grafo, origem, destino, aresta_obter_comprimento);
+}
 
-    inicializaDijkstra(grafo, infos, origem);
-    Vertice vertice;
-
-    while (existeAberto(grafo, infos)) {
-        int origem_atual = menorVelocidade(grafo, infos);
-        infos[origem_atual].aberto = false;
-
-        vertice = grafo_obter_vertice_por_indice(grafo, origem_atual);
-
-        relaxa_velocidade(grafo, infos, grafo_obter_tabela(grafo), vertice);
-    }
-
-    Pilha pilha_caminho = pilha_criar(free);
-
-    if (pilha_caminho == NULL) {
-        LOG_ERRO("Não foi possivel alocar memória para pilha\n");
-    }
-
-    int diferenca_destino_origem = abs(destino - origem);
-    int aux = destino;
-    for (int i = 0; i < diferenca_destino_origem; i++) {
-        if (aux == origem) {
-            break;
-        }
-        pilha_inserir(pilha_caminho, aux);
-        aux = infos[aux].predecessor;
-    }
+Pilha dijkstra_velocidade(Grafo grafo, int origem, int destino) {
+    return dijkstra(grafo, origem, destino, aresta_obter_velocidade);
 }
--- src/Utils/dijkstra.h
+++ src/Utils/dijkstra.h
@@ -4,26 +4,28 @@
 /*
 Implementa o algoritmo de Dijkstra, responsável por encontrar o menor caminho entre dois vértices em
 um grafo. O caminho pode ser obtido com base no comprimento (dijkstra_distancia) ou na velocidade
-(dijkstra_velocidade) das arestas. Retorna uma pilha com os índices do grafo, formando o caminho da
-origem até o destino. O usuário é responsável por liberar a memória alocada para a pilha
+(dijkstra_velocidade) das arestas. Retorna uma pilha com os vértices do grafo, formando o caminho da
+origem até o destino.
+O usuário é responsável por liberar a memória alocada para a pilha.
 */
 
 #include "../Estruturas/grafo.h"
 #include "../Estruturas/pilha.h"
 
 /*
-Retorna uma pilha com os indices referentes ao menor caminho entre os vértices de indice "origem" e
+Retorna uma pilha com os vértices referentes ao menor caminho entre os vértices de indice "origem" e
 "destino".
 O menor caminho é obtido com base no comprimentos das arestas do grafo.
 O parâmetro grafo não pode ser nulo e os índices "origem" e "destino" devem ser válidos.
 */
-Pilha dijkstra_distancia(Grafo *grafo, int origem, int destino);
+Pilha dijkstra_distancia(Grafo grafo, int origem, int destino);
 
 /*
-Retorna uma pilha com os indices referentes ao menor caminho entre os vértices de indice "origem" e
+Retorna uma pilha com os vértices referentes ao menor caminho entre os vértices de indice "origem" e
 "destino".
 O menor caminho é obtido com base na velocidade das arestas do grafo.
 O parâmetro grafo não pode ser nulo e os índices "origem" e "destino" devem ser válidos.
 */
-Pilha dijkstra_velocidade(Grafo *grafo, int origem, int destino);
+Pilha dijkstra_velocidade(Grafo grafo, int origem, int destino);
+
 #endif
